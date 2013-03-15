baf20c81b1036cc5708fd2284f92a9f5a85d9576
Steve Tinney <stinney@sas.upenn.edu>
2013-03-15 11:43:29 -0700

    use -f arg to chmod to ignore errors

--- misc/admin/userperm.sh
+++ misc/admin/userperm.sh
@@ -5,11 +5,11 @@ if [ "$1" = "" ]; then
 fi
 if [ -d ${ORACC_HOME}/$1 ]; then
     cd ${ORACC_HOME}/$1
-    chmod -R u+rw .
-    chmod -R g+r .
-    chmod -R o-rw *
+    chmod -fR u+rw .
+    chmod -fR g+r .
+    chmod -fR o-rw *
     cd ${ORACC}
-    chmod -R o+r */$1
+    chmod -fR o+r */$1
 else
     echo "userperm.sh: no such project '$1'"
     exit 1
