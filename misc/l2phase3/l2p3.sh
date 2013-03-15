cb52feb32a8f3e6a5ee15b5cfc5596d87796ffb2
Steve Tinney <stinney@sas.upenn.edu>
2013-03-15 12:30:46 -0700

    suppress error message when no g2c files are present

--- misc/l2phase3/l2p3.sh
+++ misc/l2phase3/l2p3.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+shopt -s nullglob
 webdir=$1
 shift 1
 if [ "$webdir" == "" ]; then
@@ -6,9 +7,13 @@ if [ "$webdir" == "" ]; then
     exit 1
 fi
 rm -fr $webdir/cbd ; mkdir -p $webdir/cbd
-for g2c in `ls 01bld/*/*.g2c` ; do
-    ldir=`dirname $g2c`
-    l=`basename $ldir`
-    echo producing web version of $l
-    l2-glomanager.plx -webdir=$webdir -conf l2p3.xcf -cbdlang $l $*
-done
+g2c=`echo -n 01bld/*/*.g2c`
+if [ "$g2c" != "" ]; then
+    for g2c in `ls 01bld/*/*.g2c` ; do
+	ldir=`dirname $g2c`
+	l=`basename $ldir`
+	echo producing web version of $l
+	l2-glomanager.plx -webdir=$webdir -conf l2p3.xcf -cbdlang $l $*
+    done
+fi
+
