abd9fd854b4092804e4c393c3c0a83d019ea0766
Steve Tinney <stinney@sas.upenn.edu>
2013-03-15 11:39:27 -0700

    new aggregators for editions and links

---
+++ misc/agg/agg-editions.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+oraccid=`id -u oracc`
+if [ "$EUID" != "$oraccid" ]; then
+    echo aggregation can only be done by the 'oracc' user
+    exit 1
+fi
+
+outxtf=$ORACC/agg/master-xtf.lst
+rm -f $outxtf
+for a in `agg-list-public-projects.sh` ; do
+    if [ -r $ORACC/bld/$a/lists/have-xtf.lst ]; then
+	cat $ORACC/bld/$a/lists/have-xtf.lst >>$outxtf
+    fi
+done
+sort -u -o $outxtf $outxtf
---
+++ misc/agg/agg-linkbase.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+oraccid=`id -u oracc`
+if [ "$EUID" != "$oraccid" ]; then
+    echo aggregation can only be done by the 'oracc' user
+    exit 1
+fi
+
+outbase=$ORACC/agg/master-linkbase.xml
+echo '<linkbase>' >$outbase
+for a in `agg-list-public-projects.sh` ; do
+    if [ -r $ORACC/bld/$a/linkbase.xml ]; then
+	echo '<xsl:stylesheet version="1.0" xmlns:xsl="http://www.w3.org/1999/XSL/Transform"><xsl:output method="xml" encoding="utf-8" omit-xml-declaration="yes"/><xsl:template match="/"><xsl:copy-of select="linkbase/project"/></xsl:template></xsl:stylesheet>' \
+	    | xsltproc - $ORACC/bld/$a/linkbase.xml >>$outbase
+    fi
+done
+echo '</linkbase>' >>$outbase
