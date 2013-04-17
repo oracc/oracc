6b95c68c3a3bd357839cf2eaecc229569d3014bf
Steve Tinney <stinney@sas.upenn.edu>
2013-04-16 20:54:45 -0400

    use ENV not hard-coded paths

--- misc/est/est-project.sh
+++ misc/est/est-project.sh
@@ -9,9 +9,9 @@ mkdir -p 01bld/est
 est-projuri.sh 2>01tmp/est-projuri.log
 est-corpus.sh 2>01tmp/est-corpus.log
 est-glo.sh 2>01tmp/est-glo.log
-bld=/home/$project/01bld
-tmp=/home/$project/01tmp
-cd /home/$project/02www
+bld=$ORACC_HOME/$project/01bld
+tmp=$ORACC_HOME/$project/01tmp
+cd $ORACC_HOME/$project/02www
 rm -fr estindex
 ls -1 $bld/est/project.est >$tmp/estindex.lst
 grep -l -m1 -r 'generator. content=.Oracc ESP' * >>$tmp/estindex.lst
