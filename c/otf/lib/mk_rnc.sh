db05e8184e41fb5f107d7e06257670084ad1e7f8
Steve Tinney <stinney@sas.upenn.edu>
2013-06-22 23:04:44 -0400

    eliminate derived files from ./c and ./misc directories

--- c/otf/lib/mk_rnc.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-B=../../../misc/xdf
-C=`pwd`
-(cd ../../../doc/ns/gdl/1.0 ; perl ../$B/xdfmanager.plx -rnc gdl.xdf)
-(cd ../../../doc/ns/xtr/1.0 ; perl ../$B/xdfmanager.plx -rnc xtr.xdf)
-(cd ../../../doc/ns/xtf/1.0 ; perl ../$B/xdfmanager.plx -rnc xtf.xdf)
-(cd ../../../doc/ns/cdf/1.0 ; perl ../$B/xdfmanager.plx -rnc cdf.xdf)
-(cd ../../../doc/ns/cdf/1.0 ; perl $C/mk_cdfrnc_c.plx >$C/cdf_rnc.c)
-perl ./mk_xmlnames.plx
--- misc/admin/wwwify.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-if [ "$1" = "" ]; then
-    echo wwwify.sh: must give project name as argument
-    exit 1
-fi
-chmod -R go+r /usr/local/oracc/www/$1
