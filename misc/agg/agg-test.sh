05bf4a1d5ec8da3a24290ce66a841accf5e68d82
Steve Tinney <stinney@sas.upenn.edu>
2013-06-26 09:27:11 -0400

    fix agg-test.sh for non-single systems

--- misc/agg/agg-test.sh
+++ misc/agg/agg-test.sh
@@ -2,7 +2,8 @@
 if [ "$ORACC_MODE" = "single" ]; then
     exit 0
 fi
-if [ echo `id -Gn` | grep -q oracc ]; then
+x=`id -Gn | grep oracc`
+if [ "$x" != "" ]; then
     exit 0
 fi
 echo agg: user `whoami` is not permitted to run aggregation scripts. Stop.
