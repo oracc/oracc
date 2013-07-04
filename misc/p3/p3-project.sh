fede4697a86b4b5d9bd00fdaa07fa4394ec2c5ef
Steve Tinney <stinney@sas.upenn.edu>
2013-07-04 02:40:59 -0400

    set perms on outputs

--- misc/p3/p3-project.sh
+++ misc/p3/p3-project.sh
@@ -5,3 +5,4 @@ xsltproc --stringparam project $project \
     ${ORACC}/lib/scripts/p3-project.xsl $ORACC/lib/data/p3-template.xml | \
     sed -e "s%@@PROJECT@@%$project%" >02pub/p3.html
 sed -e "s%@@PROJECT@@%$project%" < $ORACC/lib/data/as-base.xml >02www/as.xml
+chmod o+r 02pub/p3.html 02www/as.xml
