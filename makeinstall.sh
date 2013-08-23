9846356f6c7a1bab5745fd297b1e8d3b8ad15777
Steve Tinney <stinney@sas.upenn.edu>
2013-08-23 15:14:25 -0400

    begin to sort data install

--- makeinstall.sh
+++ makeinstall.sh
@@ -1,3 +1,12 @@
 #!/bin/sh
 . ./oraccenv.sh
 make install
+cd $ORACC
+mkdir -p $ORACC/00any
+cd $ORACC/00any
+curl -L -o coredata.zip http://github.com/oracc/coredata/master.zip
+unzip coredata
+mkdir -p $ORACC/lib/data
+cp -a coredata-master/cats/catpnums.vec $ORACC/lib/data
+mkdir -p $ORACC/xml/ogsl
+cp -a coredata-master/sign/ogsl-sl.xml $ORACC/xml/ogsl
