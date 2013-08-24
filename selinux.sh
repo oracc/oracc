38b49cba58e8a568eae3f5484a42a52ff6a072cb
Steve Tinney <stinney@sas.upenn.edu>
2013-08-24 10:09:50 -0400

    set environment for semanage

--- selinux.sh
+++ selinux.sh
@@ -4,6 +4,7 @@ if [ "$SEM" = "" ]; then
     echo 'selinux.sh: no semanage installed (do you need to yum install policycoreutils-python?)'
     exit 1
 fi
+. ./oraccenv.sh
 /usr/sbin/semanage fcontext -a -t httpd_user_script_exec_t "$ORACC/bin(/.*)?"
 /usr/sbin/semanage fcontext -a -t httpd_sys_content_t "$ORACC/bld(/.*)?"
 /usr/sbin/semanage fcontext -a -t httpd_sys_content_t "$ORACC/lib(/.*)?"
