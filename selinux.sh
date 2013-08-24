01b869463237cc5228bfd78850d7e59297c9b7f9
Steve Tinney <stinney@sas.upenn.edu>
2013-08-24 10:46:09 -0400

    selinux tweaks

--- selinux.sh
+++ selinux.sh
@@ -9,8 +9,12 @@ function needfile {
 . ./oraccenv.sh
 FC1=/etc/selinux/targeted/modules/active/file_contexts
 FC2=/etc/selinux/targeted/contexts/files/file_contexts
+# .local can be empty so first check for file_context
 needfile $FC1
 needfile $FC2
+# now switcheroo to .local versions
+FC1=/etc/selinux/targeted/modules/active/file_contexts.local
+FC2=/etc/selinux/targeted/contexts/files/file_contexts.local
 [ -w $FC1 ] || quit $FC1 is not writable
 [ -w $FC2 ] || quit $FC2 is not writable
 grep oracc $FC1 && quit Oracc already installed in $FC1
