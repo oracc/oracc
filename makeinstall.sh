521fd9cb52656a1de67ed0a3817bd6941ed616e9
Steve Tinney <stinney@sas.upenn.edu>
2013-08-23 20:29:56 -0400

    install doc2 from coredata

--- makeinstall.sh
+++ makeinstall.sh
@@ -10,3 +10,5 @@ mkdir -p $ORACC/lib/data
 cp -a coredata-master/cats/catpnums.vec $ORACC/lib/data
 mkdir -p $ORACC/xml/ogsl
 cp -a coredata-master/sign/ogsl-sl.xml $ORACC/xml/ogsl
+(cd coredata-master ; cp -a doc2 $ORACC/www)
+chmod -R o+r $ORACC/lib/data $ORACC/xml/ogsl $ORACC/www/doc2
