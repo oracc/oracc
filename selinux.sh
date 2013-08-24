05590ecb54e0ac72a5118e4b0157982d7bc563b2
Steve Tinney <stinney@sas.upenn.edu>
2013-08-24 10:39:19 -0400

    minor shell syntax fixes

--- selinux.sh
+++ selinux.sh
@@ -4,15 +4,15 @@ function quit {
     exit 1;
 }
 function needfile {
-    -r $1 || quit no such file $1
+    [ -r $1 ] || quit no such file $1
 }
 . ./oraccenv.sh
 FC1=/etc/selinux/targeted/modules/active/file_contexts
 FC2=/etc/selinux/targeted/contexts/files/file_contexts
 needfile $FC1
 needfile $FC2
--w $FC1 || quit $FC1 is not writable
--w $FC2 || quit $FC2 is not writable
+[ -w $FC1 ] || quit $FC1 is not writable
+[ -w $FC2 ] || quit $FC2 is not writable
 grep oracc $FC1 && quit Oracc already installed in $FC1
 grep oracc $FC2 && quit Oracc already installed in $FC2
 mkdir -p $ORACC/{bld,xml,pub}
