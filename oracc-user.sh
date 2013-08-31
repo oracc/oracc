bd22dd9c14b32d1af64996432b2d945df84db044
Steve Tinney <stinney@sas.upenn.edu>
2013-08-31 08:41:46 -0400

    Install doc/script cleanups

--- oracc-user.sh
+++ oracc-user.sh
@@ -7,5 +7,16 @@ uname=`uname`
 if [ "$uname" = "Linux" ]; then
     sudo useradd oracc
     passwd oracc
+else
+    if [ ! -d /Users/oracc ]; then
+	fail 'oracc-user.sh: please create the oracc user on this machine before proceeding'
+    fi
 fi
-su oracc oracc-source.sh
+cp oraccenv.example oraccenv.sh
+cat <<EOF
+#########################################################################
+#                                                                       #
+#  You now need edit oraccenv.sh as explained in ORACC-INSTALL          #
+#                                                                       #
+#########################################################################
+EOF
