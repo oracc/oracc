0a99982402f924a12a291c4564d8bd7cda7172a0
Steve Tinney <stinney@sas.upenn.edu>
2013-08-31 08:49:26 -0400

    need to run passwd in sudo

--- oracc-user.sh
+++ oracc-user.sh
@@ -6,7 +6,7 @@ function fail {
 uname=`uname`
 if [ "$uname" = "Linux" ]; then
     sudo useradd oracc
-    passwd oracc
+    sudo passwd oracc
 else
     if [ ! -d /Users/oracc ]; then
 	fail 'oracc-user.sh: please create the oracc user on this machine before proceeding'
