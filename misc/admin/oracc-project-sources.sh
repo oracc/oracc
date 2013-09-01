a2152bd748a53b57c20da0362841271c96d58edd
Steve Tinney <stinney@sas.upenn.edu>
2013-09-01 15:50:09 -0400

    minor adjustments to cloud scripts

--- misc/admin/oracc-project-sources.sh
+++ misc/admin/oracc-project-sources.sh
@@ -1,30 +1,30 @@
 #!/bin/sh
+function fail {
+    echo oracc-project-sources.sh: $*. Stop.
+    exit 1
+}
 proj=$1
 if [ "$proj" == "" ]; then
     echo oracc-project-sources.sh: must give project on command line
     exit 1
 fi
-cd $ORACC_HOME
-if [ ! -d $proj ]; then
-    echo oracc-project-sources.sh: no such project $proj. Stop.
-    exit 1
-fi
+cd $ORACC/$proj || fail "no such project $proj"
 fproj=`/bin/echo -n $proj | tr / -`
-tarball=$ORACC/01tmp/$fproj.tar.gz
-tar --exclude $proj/00any \
-    --exclude $proj/00bak \
-    --exclude $proj/00web/esp/backups \
-    --exclude $proj/00web/esp/httproot_changed \
-    --exclude $proj/00web/esp/httproot_changed_new \
-    --exclude $proj/00web/esp/httproot_dev \
-    --exclude $proj/00web/esp/httproot_gone \
-    --exclude $proj/00web/esp/httproot_gone_new \
-    --exclude $proj/00web/esp/httproot_live \
-    --exclude $proj/00web/esp/httproot_live_new \
-    --exclude $proj/00web/esp/temporary-files \
+tarball=01tmp/$fproj.tar.gz
+echo oracc-project-sources.sh: creating $tarball
+tar --exclude 00any \
+    --exclude 00bak \
+    --exclude 00web/esp/backups \
+    --exclude 00web/esp/httproot_changed \
+    --exclude 00web/esp/httproot_changed_new \
+    --exclude 00web/esp/httproot_dev \
+    --exclude 00web/esp/httproot_gone \
+    --exclude 00web/esp/httproot_gone_new \
+    --exclude 00web/esp/httproot_live \
+    --exclude 00web/esp/httproot_live_new \
+    --exclude 00web/esp/temporary-files \
     --exclude .DS_Store \
     --exclude '*~' \
     --exclude '*.bak' \
     --exclude '*.tar.gz' \
-    -zcf $tarball $proj/00*
-chown oracc:oracc $tarball
+    -zcf $tarball 00*
--- misc/admin/oracc-upload.sh
+++ misc/admin/oracc-upload.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 project=`oraccopt`
 if [ "$project" = "" ]; then
-    oracc-send.sh: unable to set project.  Stop.
+    oracc-upload.sh: unable to set project.  Stop.
 fi
 usproject=`/bin/echo -n $project | tr / _`
-oracc-project-sources.sh || exit 1
+oracc-project-sources.sh $project || exit 1
 sent_sources=${ORACC}/Dropbox/sources/$usproject.tar.gz
 this_sources=01tmp/$usproject.tar.gz
 function copysent {
@@ -14,8 +14,8 @@ function filesize {
     `stat -s $1 | cut -d' ' -f8 | cut -d= -f2`
 }
 project=`oraccopt`
-if [ "$project" = ""]; then
-    echo oracc_send: unable to set project. Stop
+if [ "$project" = "" ]; then
+    echo oracc_upload: unable to set project. Stop
 fi
 sent_size=`filesize $sent_sources`
 this_size=`filesize $this_sources`
