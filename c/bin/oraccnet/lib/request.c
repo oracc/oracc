608e43e9b6c980d1860ff3745ac3ce1941f0e077
Steve Tinney <stinney@sas.upenn.edu>
2013-09-22 06:57:33 -0400

    more Centos 6 missing includes; Mac OS X 10.8 is obviously too helpful …

--- c/bin/oraccnet/lib/request.c
+++ c/bin/oraccnet/lib/request.c
@@ -1,8 +1,11 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <stdarg.h>
+#incldue <stdio.h>
 #include <errno.h>
 #include <sys/resource.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 #include <signal.h>
 #include <string.h>
 #include <fcntl.h>
