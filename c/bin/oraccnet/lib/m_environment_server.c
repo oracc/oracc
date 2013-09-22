f8535ce642cd18ea146ddbc44ba2606617ed6bb5
Steve Tinney <stinney@sas.upenn.edu>
2013-09-22 06:51:35 -0400

    add missing stdio.h includes

--- c/bin/oraccnet/lib/m_environment_server.c
+++ c/bin/oraccnet/lib/m_environment_server.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <xmlrpc-c/base.h>
 #include <xmlrpc-c/server.h>
--- c/bin/oraccnet/lib/m_ox_client.c
+++ c/bin/oraccnet/lib/m_ox_client.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <xmlrpc-c/base.h>
