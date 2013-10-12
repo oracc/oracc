ad7f6090e695f9f0eff7751ad738298d9f1b159e
Steve Tinney <stinney@sas.upenn.edu>
2013-10-12 08:34:35 -0400

    check for missing env vars in oracc-subst

--- oracc-subst.sh
+++ oracc-subst.sh
@@ -1,5 +1,19 @@
 #!/bin/sh
+
+function check_env {
+    if [ "$2" = "" ]; then
+	echo oracc-subst.sh: environment variable $1 is not set--add it to oraccenv.sh. Stop.
+	exit 1
+    fi
+}
+
 ORACC=$1
+
+check_env_or_die ORACC_BUILDS $ORACC_BUILDS
+check_env_or_die ORACC_HOME $ORACC_HOME
+check_env_or_die ORACC_HOST $ORACC_HOST
+
+
 in=$2
 out=`basename $in .in`
 pat1="s#\@\@ORACC\@\@#${ORACC}#g"
