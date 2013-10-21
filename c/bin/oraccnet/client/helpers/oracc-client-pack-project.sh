568d10b6f6e2ac934a19229406878ca8f6a65341
Steve Tinney <stinney@sas.upenn.edu>
2013-10-21 14:51:38 -0400

    second attempt at getting xplatform tar bzip compression

--- c/bin/oraccnet/client/helpers/oracc-client-pack-project.sh
+++ c/bin/oraccnet/client/helpers/oracc-client-pack-project.sh
@@ -13,14 +13,14 @@ u=`uname`
 if [ "$uname" = "Darwin" ]; then
     COMPRESS=-y
 else
-    COMPRESS=-J
+    COMPRESS=-j
 fi
 zip=request.tar.gz ; export zip
 rm -f $zip
 # COPYFILE_DISABLE suppresses addition of ._* files under Mac OS X at least up
 # to 10.8
 COPYFILE_DISABLE=1 ; export COPYFILE_DISABLE
-find 00atf 00lib 00web -type f \
+find 00atf 00bib 00cat 00lib 00web -type f \
     -name '*.atf' \
     -or -name '*.css' \
     -or -name '*.glo' \
--- c/bin/oraccnet/client/make-distro.sh
+++ c/bin/oraccnet/client/make-distro.sh
@@ -6,9 +6,11 @@ chmod +x *-client.sh
 cp  ../include/oraccnet.h \
     ../lib/callinfo.c ../lib/file.c ../lib/generic_request.c ../lib/util.c \
     ../lib/m_*_client.c \
-    meths.c oracc-client.c result.c server-status.c *-client.sh \
+    meths.c oracc-client.c result.c server-status.c \
+    *-client.sh *-client-*.sh \
     $dir
 cp Makefile-client.* $dir
+chmod +x $dir/*.sh
 rm -f $dir.zip
 zip -r $dir.zip $dir
 find * -maxdepth 0 -type d -name 'oracc-xmlrpc*' -exec rm -fr '{}' ';'
