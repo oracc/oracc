8035d88ec3f9eebd1395104d6f8b6c6c21d17795
Steve Tinney <stinney@sas.upenn.edu>
2013-10-21 14:12:08 -0400

    adapt esp-to-esp2 to new 00res layout

--- misc/esp2/esp-to-esp2.sh
+++ misc/esp2/esp-to-esp2.sh
@@ -1,15 +1,16 @@
 #!/bin/sh
-if [ ! -d 00any ]; then
-    echo esp-to-esp2.sh: no 00any. Stop.
+if [ ! -d 00web/esp ]; then
+    echo esp-to-esp2.sh: no 00web/esp to convert. Stop
     exit 1
 fi
+mkdir -p 00any
 e2=00any/e2
 rm -fr $e2
-mkdir -p $e2/00config $e2/00static
+mkdir -p $e2/00config $e2/00res
 (cd 00web ; \
-    find . -type f | grep -v '^./esp' | tar zcf - -T - | tar -C ../00any/e2/00static -zxf - )
+    find . -type f | grep -v '^./esp' | tar zcf - -T - | tar -C ../00any/e2/00res -zxf - )
 (cd 00web/esp/site-content/00web ; \
-    find . -type f  | tar zcf - -T - | tar -C ../../../../00any/e2/00static -zxf - )
+    find . -type f  | tar zcf - -T - | tar -C ../../../../00any/e2/00res -zxf - )
 (cd 00web/esp/site-content ; \
     cp -p *.xml ../../../00any/e2/00config)
 (cd 00web/esp/site-content/pages ; \
@@ -21,4 +22,8 @@ fi
 tar zcf 00any/00web.tar.gz 00web || exit
 rm -fr 00web
 mv 00any/e2 00web
+rm -fr 00res
+mv 00web/00res 00res
+mv 00res/*.* 00web
+mv 00res/w3c 00web
 echo 00web has been converted to esp2.
