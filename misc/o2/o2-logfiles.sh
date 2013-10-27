b474cd508d67ef3e37f445cd802ba7c2cd30d48e
Steve Tinney <stinney@sas.upenn.edu>
2013-10-27 07:54:28 -0400

    remove INFO lines from estindex.log

--- misc/o2/o2-logfiles.sh
+++ misc/o2/o2-logfiles.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
+if [ -r 01tmp/estindex.log ]; then
+    mv 01tmp/estindex.log 01tmp/estindex.logx
+    grep -v INFO 01tmp/estindex.logx >01tmp/estindex.log
+fi
 LIST=`find 01tmp -follow -type f -name '*.log' -newer 01tmp/building`
 for l in $LIST; do
     if [ -s $l ]; then
