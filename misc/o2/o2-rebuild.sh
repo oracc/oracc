f2bbff2c3233222ec6f2ee9aaf7e2c222b941c9d
Steve Tinney <stinney@sas.upenn.edu>
2013-12-19 07:20:33 -0500

    add project prebuild/rebuild/postbuild support

--- misc/o2/o2-rebuild.sh
+++ misc/o2/o2-rebuild.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 o2-cnf.sh || exit 1
-#if [ -e 00bin/rebuild.sh ]; then
-#    exec 00bin/rebuild.sh
-#fi
+if [ -e 00bin/prebuild.sh ]; then
+    00bin/prebuild.sh
+fi
+if [ -e 00bin/rebuild.sh ]; then
+    exec 00bin/rebuild.sh
+    exit 1
+fi
 type=`oraccopt . type`
 if [ "$type" == "" ]; then
     type=corpus
@@ -17,4 +21,7 @@ if [ -x $exe ]; then
 else
     o2-rebuild.sh: cannot run $exe
     exit 1
+fi
+if [ -e 00bin/postbuild.sh ]; then
+    00bin/postbuild.sh
 fi
