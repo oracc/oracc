ae29170bc8f565fd1d8503a29d0263f7f3b73d61
Steve Tinney <stinney@sas.upenn.edu>
2013-12-20 06:44:23 -0500

    ogsl typo

--- misc/ogsl/ogsl-rebuild.sh
+++ misc/ogsl/ogsl-rebuild.sh
@@ -3,5 +3,5 @@ sl-xml.plx ogsl
 sl-db.plx ogsl
 sl-index -boot
 xsltproc -stringparam project ogsl ${ORACC}/lib/scripts/sl-HTML.xsl 02xml/ogsl-sl.xml >02www/ogsl.html
-cp -p 00lib/aliases-*.txt 02pub
+cp -p 00lib/alias-*.txt 02pub
 pubfiles.sh ogsl
