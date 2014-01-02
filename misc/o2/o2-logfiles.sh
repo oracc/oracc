3c7a27d262f1944b43c2578330a73f11adc67158
oracc <stinney@Steves-MacBook-Air.local>
2014-01-02 06:53:59 -0500

    keep failed-xtr-load errors out of logfile echo

--- misc/o2/o2-logfiles.sh
+++ misc/o2/o2-logfiles.sh
@@ -7,6 +7,6 @@ LIST=`find 01tmp -follow -type f -name '*.log' -newer 01tmp/building`
 for l in $LIST; do
     if [ -s $l ]; then
 	echo '###'errors in $l:
-	cat $l
+	cat $l | grep -v 'failed.*xtr'
     fi
 done
