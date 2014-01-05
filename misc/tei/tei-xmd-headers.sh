6af038e32ae50a38ce5790a0d19df4446242b88c
oracc <stinney@Steves-MacBook-Air.local>
2014-01-04 22:31:48 -0500

    revise tei creation to avoid fs loop and use non-gz xmd for headers

--- misc/tei/tei-xmd-headers.sh
+++ misc/tei/tei-xmd-headers.sh
@@ -1,2 +1,6 @@
 #!/bin/sh
-gunzip -cd 01bld/cdlicat.xmd.gz | xsltproc $ORACC/lib/scripts/tei-xmd-headers.xsl -
+if [ -r 01bld/cdlicat.xmd.gz ]; then
+    gunzip -cd 01bld/cdlicat.xmd.gz | xsltproc $ORACC/lib/scripts/tei-xmd-headers.xsl -
+else
+    xsltproc $ORACC/lib/scripts/tei-xmd-headers.xsl 01bld/cdlicat.xmd
+fi
