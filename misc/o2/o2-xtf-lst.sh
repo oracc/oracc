3d65d2dc9b85a856d8c1f521258b9100aff0f667
Steve Tinney <stinney@sas.upenn.edu>
2014-05-07 21:07:46 -0400

    create xsf-scores.lst so score generation works

--- misc/o2/o2-xtf-lst.sh
+++ misc/o2/o2-xtf-lst.sh
@@ -6,6 +6,9 @@ if [ "$PQX" != "" ]; then
     ls -1R 01bld/[PQX]* | ls1R2lst.plx \
 	| perl -ne "s%^.*?/([PQX][0-9]{6}).*?$%$project:\$1%&&print" \
 	>01bld/lists/have-xtf.lst
+    ls -1R 01bld/[PQX]* | ls1R2lst.plx xsf \
+	| perl -ne "s%^.*?/([PQX][0-9]{6}).*?$%$project:\$1%&&print" \
+	>01bld/lists/xsf-scores.lst
 else
     cat </dev/null >01bld/lists/have-xtf.lst
 fi
