6e04deac5bc76e49c49b315fe040386e58235516
Steve Tinney <stinney@sas.upenn.edu>
2014-07-10 08:30:34 -0400

    fix bad string test

--- misc/o2/o2-xtf-lst.sh
+++ misc/o2/o2-xtf-lst.sh
@@ -2,7 +2,8 @@
 shopt -s nullglob
 project=`oraccopt . project`
 PQX="01bld/[PQX]*"
-if [ "$PQX" != "" ]; then
+if [ -s "$PQX" ]; then
+    echo searching $PQX for .xtf files ...
     ls -1R 01bld/[PQX]* | ls1R2lst.plx \
 	| perl -ne "s%^.*?/([PQX][0-9]{6}).*?$%$project:\$1%&&print" \
 	>01bld/lists/have-xtf.lst
@@ -10,5 +11,6 @@ if [ "$PQX" != "" ]; then
 	| perl -ne "s%^.*?/([PQX][0-9]{6}).*?$%$project:\$1%&&print" \
 	>01bld/lists/xsf-scores.lst
 else
-    cat </dev/null >01bld/lists/have-xtf.lst
+    rm -f 01bld/lists/have-xtf.lst
+    touch 01bld/lists/have-xtf.lst
 fi
