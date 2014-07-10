4246b33166e196a09918d0689a234da07a86a1d8
Steve Tinney <stinney@sas.upenn.edu>
2014-07-10 08:31:20 -0400

    only do this if there is a 00atf dir

--- misc/o2/o2-atf.sh
+++ misc/o2/o2-atf.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
-l2p1-pre-atf.sh
-# rebuild files that have changed
-o2-atfsources.plx
-o2-atf.plx
-LIST=`(cd 00atf ; find . -type f -name '*.atf' -print0 | xargs -0 grep -l '^@score' | head -1)`
-if [ "$LIST" != "" ]; then
-    o2-unscore.sh
+if [ -d 00atf ]; then
+    l2p1-pre-atf.sh
+    # rebuild files that have changed
+    o2-atfsources.plx
+    o2-atf.plx
+    LIST=`(cd 00atf ; find . -type f -name '*.atf' -print0 | xargs -0 grep -l '^@score' | head -1)`
+    if [ "$LIST" != "" ]; then
+	o2-unscore.sh
+    fi
 fi
