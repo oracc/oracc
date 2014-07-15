b108edd3c337bd52662f939a008611ee7b307c52
Steve Tinney <stinney@sas.upenn.edu>
2014-07-15 07:38:18 -0400

    update to oracc-server

--- create-oracc-vhost-conf.sh
+++ create-oracc-vhost-conf.sh
@@ -53,6 +53,6 @@ cat >oracc-vhost.conf <<EOF
     RewriteCond $ORACC/www/%{REQUEST_FILENAME}/index.html -f
     RewriteRule ^(.+)/?$ \$1/index.html [L]
     RewriteRule \.(?:css|js|html|xml)$ - [L]
-    RewriteRule ^(.*)$ /cgi-bin/oracc-resolver\$1 [T=application/x-httpd-cgi,L]
+    RewriteRule ^(.*)$ /cgi-bin/oracc-server\$1 [T=application/x-httpd-cgi,L]
 </VirtualHost>
 EOF
