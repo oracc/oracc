6cb471bf48e5129d39ff7bcacef711c197feafa0
Steve Tinney <stinney@sas.upenn.edu>
2014-07-15 16:39:23 -0400

    move ogsl-rebuild to o2 where all PROJ-rebuilds will go in future

---
+++ misc/o2/ogsl-rebuild.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+sl-xml.plx ogsl
+sl-db.plx ogsl
+sl-index -boot
+xsltproc -stringparam project ogsl ${ORACC}/lib/scripts/sl-HTML.xsl 02xml/ogsl-sl.xml >02www/ogsl.html
+cp -p 00lib/alias-*.txt 02pub
+pubfiles.sh ogsl
