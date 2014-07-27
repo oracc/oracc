9d2dad8636689cd149931a1198ee8e73cdea12c6
Steve Tinney <stinney@sas.upenn.edu>
2014-07-27 11:33:08 -0400

    add some js to validate the signlist form text input

--- misc/sl/sl-signlist.sh
+++ misc/sl/sl-signlist.sh
@@ -10,6 +10,7 @@ project=`oraccopt`
 (cd signlist ;
     mkdir -p 00lib ;
     mkdir -p 00res/css ;
+    mkdir -p 00res/js ;
     mkdir -p 00web/00config ;
     mkdir -p 01tmp ;
     ln -sf $ORACC/bld/$project/signlist 01bld
@@ -21,6 +22,7 @@ cp -a 00lib/signlist-config.xml signlist/00lib/config.xml
 cp -a 00lib/signlist-parameters.xml signlist/00web/00config/parameters.xml
 cp -a 00lib/signlist-home.xml signlist/00web/home.xml
 cp -a 00lib/signlist-projesp.css signlist/00res/css/projesp.css
+cp -a 00lib/signlist-projesp.js signlist/00res/js/projesp.js
 xsltproc $ORACC/lib/scripts/sl-ESP-structure.xsl 02xml/sl-grouped.xml >signlist/00web/00config/structure.xml
 xsltproc $ORACC/lib/scripts/sl-ESP-letters.xsl 02xml/sl-grouped.xml
 xsltproc $ORACC/lib/scripts/sl-ESP-signs.xsl 02xml/sl-grouped.xml
