605ec2ada3209b9186ba91b3504225bd0e802de8
Steve Tinney <stinney@sas.upenn.edu>
2014-08-11 07:38:15 -0400

    remove old chmod

--- misc/admin/userperm.sh
+++ misc/admin/userperm.sh
@@ -8,8 +8,11 @@ if [ -d ${ORACC_HOME}/$1 ]; then
     chmod -fR u+rw .
     chmod -fR g+r .
     chmod -fR o-rw *
-    cd ${ORACC}
-    chmod -fR o+r */$1
+# This is from the old days when a project was create with links
+# into /usr/local/oracc vel sim: nowadays that is handled by the
+# build process
+#    cd ${ORACC}
+#    chmod -fR o+r */$1
 else
     echo "userperm.sh: no such project '$1'"
     exit 1
