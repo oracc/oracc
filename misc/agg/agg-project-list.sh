4ff07a6e1e50789dc4c1f4cc2608736f2c6b08c8
stinney <stinney@upenn.edu>
2017-01-30 12:25:06 -0500

    fix geojson bug; issue with project listing

--- misc/agg/agg-project-list.sh
+++ misc/agg/agg-project-list.sh
@@ -19,6 +19,7 @@ echo '</projects>' >>all-projects.xml
 xsltproc --xinclude $xsl/agg-public-projects.xsl \
     all-projects.xml >public-projects.xml
 
+agg-projects-json.plx
 xsltproc $xsl/xpd-JSON.xsl public-projects.xml >${ORACC}/www/projectlist.json
 
 ${ORACC}/bin/agg-thumbs.sh
