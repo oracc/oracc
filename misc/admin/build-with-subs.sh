3c87ddc77c14030d6b225ec97cf624166c9c210b
Steve Tinney <stinney@upenn.edu>
2017-09-08 07:54:20 -0400

    new convenience script build-with-subs.sh

---
+++ misc/admin/build-with-subs.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+subs=`find . -type d -name 00lib -exec ls '{}/config.xml' ';' | sed 's#/00lib/config.xml##g' | grep -v 00any | sed 's#^./##g' | grep -v '^.$'`
+
+oracc build clean
+for a in $subs ; do (cd $a ; oracc build clean) ; done
+oracc build clean
