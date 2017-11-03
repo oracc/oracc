18ff152bd5d26671c3bac135f9d3c367d1c82576
Steve Tinney <stinney@upenn.edu>
2017-11-03 08:53:13 -0400

    revise cdli-rebuild to new cdli git repo org

--- misc/o2/cdli-rebuild.sh
+++ misc/o2/cdli-rebuild.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 echo Rebuilding CDLI with cdli-rebuild.sh ...
 (cd 00git ; git pull)
-(cd 00cat ; unzip -o ../00git/cdli_catalogue.csv.zip)
-(cd 00src ; unzip -o ../00git/cdliatf_unblocked.atf.zip)
-head -1 00cat/cdli_catalogue.csv >00lib/fieldnames.row
-00bin/split-atf.plx
+#(cd 00cat ; unzip -o ../00git/cdli_catalogue.csv.zip)
+#(cd 00src ; unzip -o ../00git/cdliatf_unblocked.atf.zip)
+head -1 00git/cdli_catalogue_1of2.csv >00lib/fieldnames.row
+#00bin/split-atf.plx
 #o2-catalog.sh
 o2-corpus.sh
