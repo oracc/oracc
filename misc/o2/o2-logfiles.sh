f7b00c1d2546153ada3273ac55b1707f6735f4a1
Steve Tinney <stinney@upenn.edu>
2017-11-05 07:58:12 -0500

    make oracc.log less noisy (I)

--- misc/o2/o2-logfiles.sh
+++ misc/o2/o2-logfiles.sh
@@ -3,10 +3,12 @@ if [ -r 01tmp/estindex.log ]; then
     mv 01tmp/estindex.log 01tmp/estindex.logx
     grep -v INFO 01tmp/estindex.logx >01tmp/estindex.log
 fi
+rm -f 01tmp/zip
 LIST=`find 01tmp -follow -type f -name '*.log' -newer 01tmp/building`
 for l in $LIST; do
     if [ -s $l ]; then
-	echo '###'errors in $l:
-	cat $l | grep -v 'failed.*xtr'
+	wc -l $l
+#	echo '###'errors in $l:
+#	cat $l | grep -v 'failed.*xtr'
     fi
 done
