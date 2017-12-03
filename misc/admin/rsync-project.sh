f922c980926218eb151d90e4b7c9401e7ee2e4ff
Steve Tinney <stinney@upenn.edu>
2017-12-03 14:31:25 -0500

    add rsync-project

---
+++ misc/admin/rsync-project.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+project=$1
+if [[ $project = "" ]]; then
+    echo "rsync-project.sh: must give project. Stop."
+    exit 1;
+fi
+cd $ORACC_BUILDS ; echo rsyncing $project in `pwd`
+here=`hostname -f`
+if [[ $here = "build-oracc.museum.upenn.edu" ]]; then
+    there=oracc.ub.uni-muenchen.de
+    port=22000
+else
+    there=build-oracc.museum.upenn.edu
+    port=22
+fi
+src=$there:$ORACC_BUILDS/$project
+echo "rsync-project.sh: rsyncing from $src to $here"
+if [ -d $project ]; then
+    echo "rsync-project.sh: removing old version of $project"
+    sudo rm -fr $project
+fi
+rsync -lr -e "ssh -p $port" $src .
+sudo chown -R root:oracc $project
+for a in bld pub tmp www xml ; do
+    d=$ORACC_BUILDS/$a/$project
+    s=$there:$d
+    cd $ORACC_BUILDS/$a ;
+    if [ -d $d ]; then
+	echo "rsync-project.sh: removing old version of $d"
+	sudo rm -fr $d
+    fi
+    echo rsync $there; rsync -lr -e "ssh -p $port" $s . ;
+    sudo chown -R root:oracc $d
+done
