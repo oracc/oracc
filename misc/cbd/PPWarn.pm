04ca36def89ca7a29fb560c20f92969189bb9354
stinney <stinney@sas.upenn.edu>
2017-12-19 21:27:53 -0500

    refactor diagnostics to save them then dump then sorted by line number

--- misc/cbd/PPWarn.pm
+++ misc/cbd/PPWarn.pm
@@ -1,15 +1,22 @@
 package ORACC::CBD::PPWarn;
 require Exporter;
 @ISA=qw/Exporter/;
-@EXPORT = qw/pp_file pp_line pp_status pp_trace pp_warn/;
+@EXPORT = qw/pp_diagnostics pp_file pp_line pp_status pp_trace pp_warn/;
 
 use warnings; use strict; binmode STDERR, ':utf8';
 
 $ORACC::CBD::PPWarn::trace = 0;
 
+my %errlist = ();
 my $line = 0;
 my $file = '';
 my $status = 0;
+
+sub pp_diagnostics {
+    foreach my $e (sort { $a <=> $b } keys %errlist) {
+	warn @{$errlist{$e}};
+    }
+}
 
 sub pp_file {
     $file = shift;
@@ -33,7 +40,7 @@ sub pp_trace {
 }
 
 sub pp_warn {
-    warn "$file:$line: ", @_, "\n";
+    push @{$errlist{$line}}, "$file:$line: @_\n";
     ++$status;
 }
 
