328d164530e494ed06692e6d971b44f9403bd1d9
stinney <stinney@sas.upenn.edu>
2017-12-20 23:49:49 -0500

    cbdpp/Edit save cache

--- misc/cbd/Edit.pm
+++ misc/cbd/Edit.pm
@@ -12,19 +12,47 @@ use ORACC::CBD::PPWarn;
 
 sub edit {
     my($proj,$lang,$data_ref,@cbd) = @_;
-    my @cache = cache(@cbd);
-    if (pristine($lang,@cache)) {
+    my @cache = cache_create(@cbd);
+    if (cache_check($lang,@cache)) {
 	my @edits = @{$$data_ref{'edit'}};
 	foreach my $i (@edits) {
 	    warn "Edit.pm: $i\n";
 	}
+	cache_stash($lang,@cbd);
     } else {
 	pp_warn("glossary fields have been edited directly. Stop.");
     }
     @cbd;
 }
 
-sub cache {
+sub cache_check {
+    my ($lang,@cache) = @_; 
+    my $glofile = ".cbdpp/$lang.glo";
+   my $ok = 0;
+    my $len = -s $glofile;
+    if (defined $len && $len > 0) {
+	pp_trace("Edit/ok -- $glofile len = $len");
+	my $cache = join("\n",@cache);
+	if ($len == length($cache)) {
+	    my $g = '';
+	    undef $/; open(G,$glofile); $g = <G>; close(G);
+		if ($g cmp $cache) {
+		    pp_trace("Edit/ok -- $glofile differs from new input");
+		} else {
+		    pp_trace("Edit/ok -- $glofile has not been edited");
+		    $ok = 1;
+		}
+	} else {
+	    pp_trace("Edit/ok -- $glofile different in size than new input");
+	}
+    } else {
+	pp_trace("Edit/ok -- $glofile nonexistent or empty");
+	$ok = 1;
+    }
+    $ok;
+}
+
+sub cache_create {
     my @cbd = @_;
     my @c;
     my @cache_tags = qw/entry parts sense end/;
@@ -37,31 +65,14 @@ sub cache {
     @c;
 }
 
-sub pristine {
-    my ($lang,@cache) = @_;
+sub cache_stash {
+    my $lang = shift @_;
+    my @c = cache_create(@_);
     my $glofile = ".cbdpp/$lang.glo";
-    my $pristine = 0;
-    my $len = -s $glofile;
-    if (defined $len && $len > 0) {
-	pp_trace("Edit/pristine -- $glofile len = $len");
-	my $cache = join("\n",@cache);
-	if ($len == length($cache)) {
-	    my $g = '';
-	    undef $/; open(G,$glofile); $g = <G>; close(G);
-		if ($g cmp $cache) {
-		    pp_trace("Edit/pristine -- $glofile differs from new input");
-		} else {
-		    pp_trace("Edit/pristine -- $glofile has not been edited");
-		    $pristine = 1;
-		}
-	} else {
-	    pp_trace("Edit/pristine -- $glofile different in size than new input");
-	}
-    } else {
-	pp_trace("Edit/pristine -- $glofile nonexistent or empty");
-	$pristine = 1;
-    }
-    $pristine;
+    system 'mkdir', '-p', '.cbdpp';
+    open(C,">$glofile") || die "cbdpp/Edit: can't write cache $glofile\n";
+    print C join("\n", @c), "\n";
+    close(C);
 }
 
 1;
