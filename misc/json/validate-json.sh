b6fe79f52505c973ab1e46f7a1e25b472065b95a
Steve Tinney <stinney@upenn.edu>
2018-01-09 21:36:21 -0500

    give NEO portal pages; fix bug in sortcodes.json which needed some jsonifying

--- misc/json/validate-json.sh
+++ misc/json/validate-json.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 project=`oraccopt`
 echo Validating project json for $project
-(cd 01bld/json ; 
- for a in `find . -name '*.json'` ; do
+(cd 01bld/json ;
+ jlist=$ORACC_BUILDS/$project/01tmp/json.list ;
+ find . -name '*.json' | sort >$jlist ;
+ for a in `cat $jlist` ; do
      echo Validating $a
      json-licensify.plx <$a | jq . >j
      mv j $a
