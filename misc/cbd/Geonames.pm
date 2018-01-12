61aa4279b23264db8c704a86659cc0c4731a0676
stinney <stinney@sas.upenn.edu>
2018-01-12 08:08:00 -0500

    code next-gen geonames impl in Geonames.pm

--- misc/cbd/Geonames.pm
+++ misc/cbd/Geonames.pm
@@ -12,28 +12,141 @@ use ORACC::CBD::PPWarn;
 
 my @pleiades_data = ();
 
+my @geo_keys = qw/cfgwpos loctype cf alias id coord uid/;
+
 sub geonames {
     my($cbdgeo,$geos,@cbd) = @_;
     if ($#$geos >= 0) {
 	pleiades_init();
+	# geonames_load('geonames');
+	# geonames_load('.');
+	geonames_init();
 	foreach my $g (@$geos) {
 	    my $cfgwpos = $cbd[$g];
 	    $cfgwpos =~ s/^.*?\s+(\S+)\s+\[(.*?)\]\s+(\S+)\s*$/$1\[$2\]$3/;
-	    my $pl_id = ORACC::XMD::Pleiades::pleiades_id('',
-							  $cfgwpos, 
-							  @pleiades_data);
-	    if ($pl_id =~ /^0$/) {
-		if ($cbdgeo eq 'yes') {
-		    pp_line($g);
-		    pp_warn("no geonames data for $cfgwpos");
-		}
-	    } else {
-		$cbd[$g] .= "\n\@pl_id $pl_id";
-	    }
+	    my %geo_tags = get_tags($g,@cbd);
+	    $geo_tags{'o'} = $cfgwpos;
+	    geonames_augment(\%geo_tags);
+#	    my $pl_id = ORACC::XMD::Pleiades::pleiades_id('',
+#							  $cfgwpos, 
+#							  @pleiades_data);
+#	    if ($pl_id =~ /^0$/) {
+#		if ($cbdgeo eq 'yes') {
+#		    pp_line($g);
+#		    pp_warn("no geonames data for $cfgwpos");
+#		}
+#	    } else {
+#		my %pl_data = geonames_data($pl_id);
+#		foreach my $p (keys %pl_data) {
+#		    $geo_tags{$p} = $pl_data{$p}
+#		    unless defined $pl_data{$p};
+#		}
+	    $cbd[$g] .= "\n".geonames_cbd_data(%geo_tags);
+	    geonames_save_data(%geo_tags);
 	}
+	geonames_term();
     }
 
     @cbd;
+}
+
+sub get_tags {
+    my($i,@cbd) = @_;
+    my %tags = ();
+    while ($cbd[$i] !~ /^\@end\s+entry/) {
+	if (/^\@([a-z0-9_]+)\s/) {
+	    my $t = $1;
+	    if (exists $geotags{$t}) {
+		my($v) = ($cbd[$i] =~ /\@(\S+)\s+(.*)\s*$/);
+		$t =~ s/^\@//;
+		$t{$t} = $v;
+		$cbd[$i] = "\000";
+	    } else if ($t eq 'prop') {
+		my($k) = ($cbd[$i] =~ /\s(\S+)\s/);
+		if ($k && $k eq 'loctype') {
+		    $t{"prop/$k"} = $cbd[$i];
+		    $cbd[$i] = "\000";
+		}
+	    }
+	}
+    }
+    (%tags);
+}
+
+sub geonames_init {
+    open(GEO,'>01bld/geonames.tab');
+}
+sub geonames_save {
+    my %data = @_;
+    my @v = ();
+    foreach my $k (@geo_keys) {
+	push @v, $data{$k} || '';
+    }
+    print GEO join("\t",@v), "\n";
+}
+sub geonames_term {
+    close(GEO);
+}
+sub geonames_load {
+    my $argproj = shift;
+    my %data = ();
+    if ($argproj eq '.') {
+	geonames_load_project('00lib/geonames',\%data);
+    } else {
+	geonames_load_project("$ENV{'ORACC_BUILDS'}/$argproj/00lib/geonames",
+			      \%data);
+    }
+}
+sub geonames_load_project {
+    my($geobase,$data) = @_;
+    return if $geonames_loaded{$geobase}++;
+    if (-r "$geobase.lst") {
+	open(L,"$geobase.lst");
+	while (<L>) {
+	    chomp;
+	    my @p = split(/\s+/,$_);
+	    foreach my $p (@p) {
+		geonames_load_project($p,$data);
+	    }
+	}
+	close(L);
+    }
+    if (-r "$geobase.tab") {
+	open(T,"$geobase.tab");
+	while (<T>) {
+	    chomp;
+	    my($o) = (/^(\S+)\t/);
+	    $$data{$o} = $_; # later .tabs overwrite earlier ones
+	}
+	close(T);
+    }
+}
+sub geonames_augment {
+    my $data = shift;
+    if ($$data{'cfgwpos'}) {
+	if ($geonames{$$data{'cfgwpos'}}) {
+	    my %a = ();
+	    my @a = split(/\t/,$geonames{$$data{'cfgwpos'}});
+	    @a{@geo_keys) = @a;
+	    foreach my $k (@geo_keys) {
+		$$data{$k} = $a{$k} unless defined $$data{$k};
+	    }
+	}
+    } else {
+	warn "Geonames.pm: internal error: no cfgwpos in data\n";
+    }
+}
+sub geonames_cbd_data {
+    my $data = shift;
+    my @c = ();
+    foreach my $k ($geo_keys[1..$#geo_keys]) {
+	if ($k eq 'loctype') {
+	    push @c, "\@prop loctype $$data{$k}";
+	} else {
+	    push @c, "\@$k $$data{$k}";
+	}
+    }
+    join("\n", @c);
 }
 
 # sub find {
