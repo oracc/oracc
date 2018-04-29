682fe2bff6c8725d27ddb35248ff4c5b83ff40d5
stinney <stinney@sas.upenn.edu>
2018-04-29 14:20:50 -0400

    improve update/rebuild support for epsd2

--- misc/o2/o2-update-lem.sh
+++ misc/o2/o2-update-lem.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
-o2-cnf.sh
-l2p1-from-glos.sh
-l2p1-lemm-data.plx -update
-(cd 02pub ; 
- for a in lemm-*.new ; do
-     sig=`basename $a .new`.sig
-     mv $a $sig
- done
- )
+project=`proj-from-conf.sh`
+if [ -e $ORACC/bin/$project-update.sh ]; then
+    exec $ORACC/bin/$project-update.sh
+    exit 1
+else
+    o2-cnf.sh
+    l2p1.sh
+fi
