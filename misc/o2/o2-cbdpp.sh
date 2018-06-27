43a6fdf6887cb143e0650025da977e83587bfa0e
stinney <stinney@sas.upenn.edu>
2018-06-27 08:50:02 -0400

    add new o2 scripts

---
+++ misc/o2/o2-cbdpp.sh
@@ -0,0 +1,35 @@
+#/bin/sh
+
+projtype=`oraccopt . type`
+g2=`oraccopt . g2`
+if [ "$g2" == "yes" ]; then
+    if [ -d "00src" ]; then
+	srcglo=`ls -1 00src/*.glo | grep -v '~' | grep glo`
+    else
+	srcglo=""
+    fi
+    for a in $srcglo ; do
+	if [ "$projtype" == "superglo" ]; then
+	    cbdpp.plx -nosigs -announce $a
+	    # there can only be one .glo in a superglo so print its name and exit
+	    /bin/echo -n $a | sed s/00src/01tmp/
+	    exit 0
+	else
+	    cbdpp.plx -announce $a
+	fi
+    done
+    libglo=`ls -1 00lib/*.glo | grep -v '~' | grep glo`
+    for a in $libglo ; do
+	globase=`basename $a`
+	if [ ! -r "00src/$globase" ]; then
+	    if [ "$projtype" == "superglo" ]; then
+		cbdpp.plx -nosigs -announce $a
+		# there can only be one .glo in a superglo so print its name and exit
+		/bin/echo -n $a | sed s/00lib/01tmp
+		exit 0
+	    else
+		cbdpp.plx -announce $a
+	    fi
+	fi
+    done
+fi
---
+++ misc/o2/o2-super-dyn.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+# For any project in 00lib/superdyn.lst update the map file
+# dynamically
+superglo=$1
+if [ "$superglo" == "" ]; then
+    echo "$0: must give name of super-glossary, e.g., 01tmp/sux.glo"
+    exit 1
+fi
+if [ -r 00lib/superdyn.lst ]; then
+    ext=0
+    for a in `cat 00lib/superdyn.lst`; do
+	echo Mapping $a into $superglo
+	ext=$((1+$ext))
+	cbd-super-compare.plx -apply -dynamic -incr $ext -inplace -base $superglo -proj $a
+    done
+else
+    echo "$0: no 00lib/superdyn.lst, nothing to do."
+fi
