46d1e2d6cb451f5685ea03a4bbee821f9beb40de
stinney <stinney@sas.upenn.edu>
2018-08-12 18:31:27 -0400

    add missing f2test.c

---
+++ c/otf/ox/f2test.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "hash.h"
+#include "tokenizer.h"
+#include "initterm.h"
+#include "f2.h"
+
+int
+main(int argc, char **argv)
+{
+  struct f2 *f2p = malloc(sizeof(struct f2));
+  if (argc > 1)
+    f2_parse((unsigned char *)"<arg>",0,(unsigned char *)argv[1],f2p,NULL,NULL);
+  return 0;
+}
+const char *prog = "f2test";
+const char *usage_string = "f2test [SIG]";
+int major_version = 1;
+int minor_version = 0;
+void help(void) { ; }
+int opts() { return 0; }
+int verbose = 0;
+
