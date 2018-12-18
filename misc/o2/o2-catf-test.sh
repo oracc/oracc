66c46977503f1bbd29af153a25d0f63bd47d6c96
stinney <stinney@sas.upenn.edu>
2018-12-18 18:13:15 +0100

    fix date_of_origin in P3 script; add catf test script

---
+++ misc/o2/o2-catf-test.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+fullproj=`oraccopt`
+project=`basename $fullproj`
+if [ "$project" == "" ]; then
+    echo o2-catf.sh: must be run from a project directory
+    exit
+fi
+catf=`oraccopt . catf-ok`
+if [ "$catf" = "yes" ]; then
+    fproj=`/bin/echo -n $project | tr '/' '-'`
+    ox -I01bld/atfsources.lst | catf-manager.plx -s 2>01tmp/catf-manager.log >02pub/$project.catf
+    echo o2-catf.sh: created 02pub/$fproj.catf
+    errcount=`grep -c ^xtf 01tmp/catf-manager.log`
+    echo 01tmp/catf-manager.log error count = $errcount
+else
+    echo o2-catf.sh: CATF output is not enabled for project $project
+fi
