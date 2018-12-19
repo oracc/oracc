f3c8dbf9c5aba9b1972cab1d426bce19f9be83fd
stinney <stinney@sas.upenn.edu>
2018-12-19 15:07:31 +0100

    try fixing catf git update for host build-oracc

--- misc/o2/o2-catf.sh
+++ misc/o2/o2-catf.sh
@@ -12,7 +12,7 @@ if [ "$catf" = "yes" ]; then
     echo o2-catf.sh: created 02pub/$fproj.catf
     errcount=`grep -c ^xtf 01tmp/catf-manager.log`
     echo 01tmp/catf-manager.log error count = $errcount
-    host=`hostname`
+    host=`hostname | cut -d. -f1`
     if [[ $host = "build-oracc" ]]; then
 	echo Copying $fproj.catf to catf repo
 	cp 02pub/$fproj.catf /home/oracc/git/catf
@@ -21,7 +21,11 @@ if [ "$catf" = "yes" ]; then
 	sudo -u stinney scp 02pub/$fproj.catf stinney@build-oracc.museum.upenn.edu:/home/oracc/git/catf	
     fi
     echo Updating github repo github.com/oracc/catf
-    sudo -u stinney ssh build-oracc.museum.upenn.edu ./git-oracc-catf.sh $fproj.catf
+    if [[ $host = "build-oracc" ]]; then
+	sudo -u stinney ./git-oracc-catf.sh $fproj.catf
+    else
+	sudo -u stinney ssh build-oracc.museum.upenn.edu ./git-oracc-catf.sh $fproj.catf
+    fi
 else
     echo o2-catf.sh: CATF output is not enabled for project $fullproject
 fi
