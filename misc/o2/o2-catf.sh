fb9eb7d72480229a5a636af9928983606831fc5f
stinney <stinney@sas.upenn.edu>
2018-12-19 15:17:55 +0100

    try fixing catf git update for host build-oracc

--- misc/o2/o2-catf.sh
+++ misc/o2/o2-catf.sh
@@ -12,6 +12,7 @@ if [ "$catf" = "yes" ]; then
     echo o2-catf.sh: created 02pub/$fproj.catf
     errcount=`grep -c ^xtf 01tmp/catf-manager.log`
     echo 01tmp/catf-manager.log error count = $errcount
+    echo '(In case of problems try: less /home/oracc/'$fullproj'/01tmp/catf-manager.log)'
     host=`hostname | cut -d. -f1`
     if [[ $host = "build-oracc" ]]; then
 	echo Copying $fproj.catf to catf repo
@@ -22,7 +23,7 @@ if [ "$catf" = "yes" ]; then
     fi
     echo Updating github repo github.com/oracc/catf
     if [[ $host = "build-oracc" ]]; then
-	sudo -u stinney /home/stinney/git-oracc-catf.sh $fproj.catf
+	sudo -i -u stinney ./git-oracc-catf.sh $fproj.catf
     else
 	sudo -u stinney ssh build-oracc.museum.upenn.edu ./git-oracc-catf.sh $fproj.catf
     fi
