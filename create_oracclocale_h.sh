68c16341d4f30d51c36b00c7433ebca315ae3ddb
Steve Tinney <stinney@upenn.edu>
2019-01-15 17:57:50 -0500

    remove testutf8 from bin/util and fix oracclocale.sh

--- create_oracclocale_h.sh
+++ create_oracclocale_h.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 ORACC_LOCALE=`locale -a | grep ^en | grep -i utf | head -1`
 cat >c/include/oracclocale.h <<EOF
-#define ORACC_LOCALE $ORACC_LOCALE
+#define ORACC_LOCALE "$ORACC_LOCALE"
 EOF
 
