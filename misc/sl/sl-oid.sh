9e61adb456d7d80a992b01b13e7d508b5e4c03ec
Steve Tinney <stinney@upenn.edu>
2019-02-17 11:37:49 -0500

    better SL OID generation

--- misc/sl/sl-oid.sh
+++ misc/sl/sl-oid.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
-grep ^@sign 00lib/*.asl | perl -p -e 's/^.*?\s+(.*?)$/sl\t$1\tsign/' | oid.plx -a -d sl -p ogsl >02pub/sl-oid.tab
+grep ^@sign 00lib/*.asl | perl -p -e 's/^.*?\s(\S+)$/sl\t$1\tsign/' | sort -u >01tmp/oid-sign.tab
+grep ^@form 00lib/*.asl | perl -p -e 's/^\S+\s+\S+\s+(\S+).*$/sl\t$1\tsign/' | sort -u >01tmp/oid-form.tab
+cat 01tmp/oid-{sign,form}.tab | oid.plx -a -d sl -p ogsl >02pub/sl-oid.tab
