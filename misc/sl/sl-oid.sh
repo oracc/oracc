432b162e3765a40cf514b9a51212fa519e030c5b
Steve Tinney <stinney@upenn.edu>
2019-02-17 12:12:56 -0500

    uniq sl-oid.sh properly

--- misc/sl/sl-oid.sh
+++ misc/sl/sl-oid.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-grep ^@sign 00lib/*.asl | perl -p -e 's/^.*?\s(\S+)$/sl\t$1\tsign/' | sort -u >01tmp/oid-sign.tab
-grep ^@form 00lib/*.asl | perl -p -e 's/^\S+\s+\S+\s+(\S+).*$/sl\t$1\tsign/' | sort -u >01tmp/oid-form.tab
-cat 01tmp/oid-{sign,form}.tab | oid.plx -a -d sl -p ogsl >02pub/sl-oid.tab
+grep ^@sign 00lib/*.asl | perl -p -e 's/^.*?\s(\S+)$/sl\t$1\tsign/' >01tmp/oid-sign.tab
+grep ^@form 00lib/*.asl | perl -p -e 's/^\S+\s+\S+\s+(\S+).*$/sl\t$1\tsign/' >01tmp/oid-form.tab
+sort -u 01tmp/oid-{sign,form}.tab | oid.plx -a -d sl -p ogsl >02pub/sl-oid.tab
