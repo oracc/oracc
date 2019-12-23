5e935e627783dd449042dadf98985fd14a0d89e7
Steve Tinney <stinney@upenn.edu>
2019-12-23 17:09:53 +0000

    remove width:100 from screen.css and add some admin scripts

---
+++ misc/admin/apply-oracc.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+what=$1
+if [ "$what" == "" ]; then
+    echo "$0: must say which oracc command, e.g., update. Stop"
+    exit 1
+fi
+for a in `list-all-subs.sh` ; do
+    (cd $a ; oracc $what)
+done
+oracc . $what
