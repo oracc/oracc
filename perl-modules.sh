05016593ebe5e818405e1d62e66330032b1a2b87
Steve Tinney <stinney@upenn.edu>
2020-04-05 22:53:13 -0400

    note in perl-modules.sh to remember to move to new install approach

--- perl-modules.sh
+++ perl-modules.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 #export PERL_MM_USE_DEFAULT=1
+
+# should switch to cpanm (apt install cpanminus) and use
+#
+# cpanm -S Log::Log4perl RTF::Writer Text::CSV_XS String::Similarity DateTime Clone::PP
+
 cpan install Log::Log4perl RTF::Writer Text::CSV_XS String::Similarity DateTime Clone::PP
