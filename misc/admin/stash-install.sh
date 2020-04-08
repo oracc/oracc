a7b1980a92f7d5e347e6aae43e0f42a262c95703
Steve Tinney <stinney@upenn.edu>
2020-04-08 09:47:14 -0400

    work on stash-install

--- misc/admin/stash-install.sh
+++ misc/admin/stash-install.sh
@@ -20,15 +20,16 @@ tarball=stash/srv/$projfile.tar.xz
 if [ -r $tarball ]; then
     echo "Installing $tarball"
     tar -C $ORACC_BUILDS -Jxf $tarball
-    mkdir -p $ORACC/tmp/$project
-    mkdir -p $project
-    cd $project
+    cd $ORACC_BUILDS/$project
+# why this mkdir ?
+#    mkdir -p $project
+    mkdir -p tmp/$project
     rm -f 01bld 01tmp 02pub 02www 02xml
-    ln -sf $ORACC/bld/$project 01bld
-    ln -sf $ORACC/tmp/$project 01tmp
-    ln -sf $ORACC/pub/$project 02pub
-    ln -sf $ORACC/www/$project 02www
-    ln -sf $ORACC/xml/$project 02xml
+    ln -sf $ORACC_BUILDS/bld/$project 01bld
+    ln -sf $ORACC_BUILDS/tmp/$project 01tmp
+    ln -sf $ORACC_BUILDS/pub/$project 02pub
+    ln -sf $ORACC_BUILDS/www/$project 02www
+    ln -sf $ORACC_BUILDS/xml/$project 02xml
     exit 0
 else
     echo "stash-install.sh: failed to find $project tarball $tarball"
