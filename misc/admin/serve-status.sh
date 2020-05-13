73b62469dcf1797650a9862161d5dbd8e8f3efe8
Steve Tinney <stinney@upenn.edu>
2020-05-13 23:43:01 +0000

    remove derived file serve-status.sh from repo

--- misc/admin/serve-status.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-for a in /Users/stinney/orc/www/srv/*.tar.xz ; do
-    sec=`stat -c %Y $a`
-    prj=`basename $a .tar.xz`
-    prj=`/bin/echo -n $prj | tr - /`
-    echo "$prj	$sec"
-done
