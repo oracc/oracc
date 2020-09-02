501daaf35f612d5660eda5dcd8813b4f1f648565
Steve Tinney <stinney@upenn.edu>
2020-09-01 20:38:51 -0400

    add xmd-sessions.plx call to o2-cat.sh

--- misc/o2/o2-cat.sh
+++ misc/o2/o2-cat.sh
@@ -3,6 +3,7 @@ mkdir -p 01bld/cat
 atflinks.plx >01bld/atflinks.tab
 linknorm.plx 01bld/atflinks.tab >01bld/normlinks.tab
 translist.sh >01bld/translist.txt
+xmd-sessions.plx
 xmdmanager2.plx
 xmd-check.plx
 xsltproc ${ORACC}/lib/scripts/xmd-summaries.xsl 01bld/cdlicat.xmd >01bld/cat-sum.xml
