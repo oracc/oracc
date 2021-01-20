76cf22bdd3aad7f9c4fea271480521cfa26b5583
Steve Tinney <stinney@upenn.edu>
2021-01-20 15:50:22 -0500

    tweak cdli-rebuild to reinstate nightly cron build

--- misc/o2/cdli-rebuild.sh
+++ misc/o2/cdli-rebuild.sh
@@ -1,6 +1,20 @@
 #!/bin/sh
+#
+# As of 2021-01-20 this just rebuilds catalog data not corpus
+#
 echo Rebuilding CDLI with cdli-rebuild.sh ...
 (cd 00git ; git pull)
-head -1 00git/cdli_catalogue_1of2.csv >00lib/fieldnames.row
-00bin/csv-clean.plx
-o2-corpus.sh
+if [ $? == 1 ]; then
+    head -1 00git/cdli_catalogue_1of2.csv >00lib/fieldnames.row
+    00bin/csv-clean.plx
+    o2-catalog.sh
+    exit 0
+else
+    d=`isogmt`
+    if [ $? == 0 ]; then
+	echo "$0 $d: git up to date, no rebuild or serve necessary."
+    else
+	echo "$0 $d: exiting on git error, no rebuild or serve."
+    fi
+    exit $?
+fi
