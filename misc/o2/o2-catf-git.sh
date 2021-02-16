72ed9c7b0253a373e87ac95de336c81e8261f6ac
Steve Tinney <stinney@upenn.edu>
2021-02-16 07:39:56 -0500

    revising catf generation procedure

---
+++ misc/o2/o2-catf-git.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+fullproj=`oraccopt`
+project=`basename $fullproj`
+if [ "$project" == "" ]; then
+    echo o2-catf.sh: must be run from a project directory
+    exit
+fi
+catf=`oraccopt . catf-ok`
+if [ "$catf" = "yes" ]; then
+    fproj=`/bin/echo -n $fullproj | tr '/' '-'`
+    ox -I01bld/atfsources.lst | catf-manager.plx -s 2>01tmp/catf-manager.log >02pub/$fproj.catf
+    echo o2-catf.sh: created 02pub/$fproj.catf
+    errcount=`grep -c ^xtf 01tmp/catf-manager.log`
+    echo 01tmp/catf-manager.log error count = $errcount
+    echo '(In case of problems try: less /home/oracc/'$fullproj'/01tmp/catf-manager.log)'
+    host=`hostname | cut -d. -f1`
+    if [[ $host = "build-oracc" ]]; then
+	echo Copying $fproj.catf to catf repo
+	cp 02pub/$fproj.catf /home/oracc/git/catf
+    else
+	echo Using scp to send $fproj.catf to build-oracc catf repo
+	sudo -u stinney scp 02pub/$fproj.catf stinney@build-oracc.museum.upenn.edu:/home/oracc/git/catf	
+    fi
+    echo Updating github repo github.com/oracc/catf
+    if [[ $host = "build-oracc" ]]; then
+	sudo -i -u stinney ./git-oracc-catf.sh $fproj.catf
+    else
+	sudo -u stinney ssh build-oracc.museum.upenn.edu ./git-oracc-catf.sh $fproj.catf
+    fi
+else
+    echo o2-catf.sh: CATF output is not enabled for project $fullproject
+fi
--- misc/o2/o2-catf.sh
+++ misc/o2/o2-catf.sh
@@ -5,28 +5,13 @@ if [ "$project" == "" ]; then
     echo o2-catf.sh: must be run from a project directory
     exit
 fi
-catf=`oraccopt . catf-ok`
-if [ "$catf" = "yes" ]; then
+#catf=`oraccopt . catf-ok`
+#if [ "$catf" = "yes" ]; then
     fproj=`/bin/echo -n $fullproj | tr '/' '-'`
-    ox -I01bld/atfsources.lst | catf-manager.plx -s 2>01tmp/catf-manager.log >02pub/$fproj.catf
-    echo o2-catf.sh: created 02pub/$fproj.catf
-    errcount=`grep -c ^xtf 01tmp/catf-manager.log`
-    echo 01tmp/catf-manager.log error count = $errcount
-    echo '(In case of problems try: less /home/oracc/'$fullproj'/01tmp/catf-manager.log)'
-    host=`hostname | cut -d. -f1`
-    if [[ $host = "build-oracc" ]]; then
-	echo Copying $fproj.catf to catf repo
-	cp 02pub/$fproj.catf /home/oracc/git/catf
-    else
-	echo Using scp to send $fproj.catf to build-oracc catf repo
-	sudo -u stinney scp 02pub/$fproj.catf stinney@build-oracc.museum.upenn.edu:/home/oracc/git/catf	
-    fi
-    echo Updating github repo github.com/oracc/catf
-    if [[ $host = "build-oracc" ]]; then
-	sudo -i -u stinney ./git-oracc-catf.sh $fproj.catf
-    else
-	sudo -u stinney ssh build-oracc.museum.upenn.edu ./git-oracc-catf.sh $fproj.catf
-    fi
-else
-    echo o2-catf.sh: CATF output is not enabled for project $fullproject
-fi
+    ox -I01bld/atfsources.lst | catf-manager.plx -s 2>01tmp/catf-manager.log >02www/project.catf
+    chown root:oracc 02www/project.catf
+    chmod +r 02www/project.catf
+    echo o2-catf.sh: you can now retrieve /$fullproj/project.catf
+#else
+#    echo o2-catf.sh: CATF output is not enabled for project $fullproject
+#fi
