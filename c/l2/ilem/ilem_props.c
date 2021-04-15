adbc63f2ac3ffac2fd83110cfaaeb2e703b526d2
Steve Tinney <stinney@upenn.edu>
2021-04-15 11:26:33 -0400

    lemprops support coded and compiled, needs debugged

--- c/l2/ilem/ilem_props.c
+++ c/l2/ilem/ilem_props.c
@@ -8,7 +8,8 @@
  *  - indented lines continue previous value set
  *  - value '*' means uncontrolled value set
  *  - value '-' means property takes no value (boolean, presence is true, absence false)
- *  - value '#' means value may be a reference (e.g., @1 ... $@1)
+ *  - value '@' means value may be a reference (e.g., @1 ... $@1)
+ *  - value '|' is reserved for system use
  *
  * Property Names:
  *
@@ -36,8 +37,14 @@
 #include "loadfile.h"
 #include "warning.h"
 
+static char *plist = "00lib/lemprops.txt";
+static int pline = 1;
+
+static int v_ok = 1;
+
 static Hash_table *h;
 static struct npool *p;
+static unsigned char *prop = NULL;
 
 static unsigned char *
 ilem_props_prop(unsigned char *s)
@@ -54,7 +61,9 @@ ilem_props_prop(unsigned char *s)
       *t = '\0';
     }
 
-  printf("prop: :%s:\n", s);
+  prop = npool_copy(s, p);
+
+  printf("prop: :%s:\n", prop);
 
   if (save)
     *t = save;
@@ -64,9 +73,105 @@ ilem_props_prop(unsigned char *s)
 }
 
 static unsigned char *
+ilem_props_npool(unsigned char *v)
+{
+  unsigned char *pv = NULL;
+  unsigned char *tmp = malloc(strlen((char*)prop)+strlen((char*)v)+2);
+  sprintf((char*)tmp,"%s=%s",prop,v);
+  pv = npool_copy(tmp,p);
+  free(tmp);
+  return pv;
+}
+
+/* prop and v are both pool copies on entry to this routine */
+static unsigned char *
+ilem_props_save(unsigned char *v)
+{
+  unsigned char *pv = NULL;
+  if (prop && v)
+    {
+      pv = ilem_props_npool(v);
+      if (hash_find(h,pv))
+	{
+	  fprintf(stderr, "%s:%d: ignoring duplicate property %s\n",
+		  plist, pline, prop);
+	}
+      else
+	{
+	  unsigned char *found = NULL;
+	  if ((found = hash_find(h,pv)))
+	    {
+	      fprintf(stderr, "%s:%d: ignoring duplicate value %s in property %s\n",
+		      plist, pline, v, prop);
+	    }
+	  else
+	    {
+	      hash_add(h,pv,&v_ok);
+	      fprintf(stdout, "adding pv == %s\n", pv);
+	      if ((found = hash_find(h,v)))
+		{
+		  /* This is a repeat occurrence of v 
+		   *   found is the previous occurrences
+		   *   pv    is the second occurrence
+		   */
+		  unsigned char *ambig_val = NULL, *tmp;
+		  tmp = malloc(strlen((char*)found) + strlen((char*)pv) + 2);
+		  sprintf((char*)tmp, "%s %s", found, pv);
+		  ambig_val = npool_copy(tmp,p);
+		  free(tmp);
+		  hash_add(h,v,ambig_val);
+		  fprintf(stderr, "%s:%d: ambiguous value %s requires PROP=VAL: %s\n",
+			  plist, pline, v, ambig_val);
+		  fprintf(stdout, "adding v || %s\n", ambig_val);
+		}
+	      else
+		{
+		  hash_add(h,v,pv);
+		  fprintf(stdout, "adding v => %s\n", pv);
+		}
+	    }
+	}
+    }
+  return pv;
+}
+
+static void
+ilem_props_val(unsigned char *v)
+{
+  if (v && *v)
+    {
+      if (*v == '@' || *v == '-' || *v == '*')
+	{
+	  if (!v[1])
+	    {
+	      unsigned char *pv = ilem_props_save(v);
+	      fprintf(stderr, "%s:%d: pv: %s\n", plist, pline, pv);
+	    }
+	  else
+	    {
+	      fprintf(stderr, "%s:%d: property %s has bad value %s (junk after @, *, or -)\n",
+		      plist, pline, prop, v);
+	    }
+	}
+      else if (*v == '|' && !v[1])
+	{
+	  fprintf(stderr, "%s:%d: in prop %s standalone value '|' is not allowed (reserved for system use)\n",
+		  plist, pline, prop);
+	}
+      else
+	{
+	  unsigned char *pv = NULL;
+	  v = npool_copy(v,p);
+	  pv = ilem_props_save(v);
+	  fprintf(stderr, "%s:%d: pv: %s\n", plist, pline, pv);
+	}
+    }
+}
+
+static unsigned char *
 ilem_props_values(unsigned char *s)
 {
-  unsigned char *t = s;
+  unsigned char *t = s, *v = NULL;
   unsigned char save = '\0';
 
   while (*t && '\n' != *t)
@@ -78,7 +183,26 @@ ilem_props_values(unsigned char *s)
       *t = '\0';
     }
 
-  printf("values: %s\n", s);
+  v = s;
+  while (*v)
+    {
+      unsigned char *w = NULL, wsave = '\0';
+      while (' ' == *v || '\t' == *v)
+	++v;
+      w = v;
+      while (*w && ' ' != *w && '\t' != *w)
+	++w;
+      if (*w)
+	{
+	  wsave = *w;
+	  *w = '\0';
+	}
+      ilem_props_val(v);
+      if (wsave)
+	*w = wsave;
+      v = w;
+    }
+
   if (save)
     *t = save;
 
@@ -88,9 +212,6 @@ ilem_props_values(unsigned char *s)
 static void
 ilem_props_load(void)
 {
-  char *plist = "00lib/lemprops.txt";
-  int pline = 1;
-  unsigned char *tok = NULL, *val = NULL;
   if (!access(plist, R_OK))
     {
       unsigned char *f = loadfile((unsigned char *)plist, NULL);
@@ -114,11 +235,10 @@ ilem_props_load(void)
 	      break;
 	    case '\n':
 	      ++pline;
-	      tok = NULL;
 	      ++s;
 	      break;
 	    default:
-	      if (tok == NULL && (*s > 127 || !isspace(*s)))
+	      if ((s == f || s[-1] == '\n') && (*s > 127 || !isspace(*s)))
 		s = ilem_props_prop(s);
 	      s = ilem_props_values(s);
 	      break;
@@ -150,9 +270,53 @@ ilem_props_term(void)
  * validate KEY/VALUE against our hash.
  */
 struct keypair *
-ilem_props_look(const char *kv)
+ilem_props_look(unsigned char *kv)
 {
   static struct keypair kp;
+  static unsigned char *tmp = NULL;
+  kp.key = kp.val = NULL;
+  if (kv)
+    {
+      unsigned char *equal = NULL;
+      if ((equal = (unsigned char *)strchr((char*)kv,'=')))
+	{
+	  if (hash_find(h,kv))
+	    {
+	      kp.key = (char*)kv;
+	      kp.val = (char*)(equal+1);
+	      *equal = '\0';
+	    }
+	}
+      else
+	{
+	  unsigned char *found = hash_find(h,kv);
+	  if (found)
+	    {
+	      if (strchr((char*)found, ' '))
+		vwarning("ambigous value requires $PROP=VAL: %s", found);
+	      else
+		{
+		  tmp = realloc((char*)tmp,strlen((char*)found)+1);
+		  strcpy((char*)tmp,(char*)found);
+		  kp.key = (char*)tmp;
+		  while (*tmp && *tmp != '=')
+		      ++tmp;
+		  *tmp = '\0';
+		  kp.val = (char*)(tmp+1);
+		}
+	    }
+	  else
+	    vwarning("$%s not found in lemprops", kv);
+	}
+    }
+  else
+    {
+      if (tmp)
+	{
+	  free(tmp);
+	  tmp = NULL;
+	}
+    }
   return &kp;
 }
 
