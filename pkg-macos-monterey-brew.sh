d94381ccf49309eb39ff2f557e64d316cfcc8d13
Stephen Tinney <stinney@maxpro22.local>
2022-02-01 23:50:58 -0500

    macos monterey install tweaks

---
+++ pkg-macos-monterey-brew.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+cd ~
+# . .profile
+brew install autoconf
+#brew install autoheader # already in OS X
+brew install automake
+brew install libtool
+brew install gdbm
+brew install pcre
+#brew install msort
+brew install xmlrpc-c
+brew install rnv
+brew install tre
+brew install utf8proc
+brew install gmp
+brew install gsoap
+cpan install Text::CSV_XS
+cpan install RTF::Writer
+cpan install String::Similarity
+# no packages for msort and libuninum on brew yet
+
