702c8e4374f7d7b4259dd2a3a152140f98c85f52
Steve Tinney <stinney@upenn.edu>
2022-03-07 22:46:03 -0500

    linkbase tweaks

--- misc/o2/o2-catalog.sh
+++ misc/o2/o2-catalog.sh
@@ -7,6 +7,12 @@ function progress {
 progress o2-cnf.sh
 progress o2-lst.sh
 progress o2-cat.sh
+if [ -r 00etc/linkbase.xml ]; then
+    cp -a 00etc/linkbase.xml 01bld
+fi
+if [ -r 01bld/linkbase.xml ]; then
+    scoregen.plx -link
+fi
 progress o2-xml.sh
 progress o2-web.sh
 progress o2-prm.sh
