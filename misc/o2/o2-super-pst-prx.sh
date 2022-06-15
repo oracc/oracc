2421f967c2c3403eeb79a4ab1aab4bc26545b8ee
Steve Tinney <stinney@upenn.edu>
2022-06-14 20:52:56 -0400

    more fixes to get epsd2/admin/names to build sensibly

---
+++ misc/o2/o2-super-pst-prx.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+# Finally, construct a virtual corpus based on the instances
+echo "o2-super-prep.sh: constructing virtual corpus"
+grep -v ^@fields 01bld/from-prx-glo.sig | cut -f2 | tr ' ' '\n' \
+    | cut -d. -f1 | sort -u | grep -v '^$' >00lib/proxy.lst
+
+catmaster=`xmdcatmaster.plx`
+if [ "$catmaster" != "" ]; then
+    echo "o2-super-prep.sh: using catalog-master-project $catmaster"
+    # perl -p -i -e "s/\$/\@$catmaster/" 00lib/proxy.lst
+    mv -f 00lib/proxy.lst proxy.bak
+    sed 's#$#@epsd2/catalogue#' <proxy.bak >00lib/proxy.lst
+    rm -f proxy.bak
+fi
+
+o2-lst.sh
+o2-cat.sh
+
+#xmd-ids.sh -c 01bld/cdlicat.xmd 01bld/lists/cat-ids.lst
+echo "o2-super-prep.sh: generating cat-ids.lst"
+xmllint --format --encode utf8 01bld/cdlicat.xmd | grep '<id_' \
+    | atfgrabpq.plx >01bld/lists/cat-ids.lst
+
+## Now trim the proxy lists to remove any text without xmd info
+echo "o2-super-prep.sh: pruning lists"
+prune-lists.plx
+
+## And trim the imported signatures as well to remove dead insts
+echo "o2-super-prep.sh: pruning sigs"
+mkdir -p 01bld/sigbak ; cp -a 01bld/*.sig 01bld/sigbak
+l2p1-prune-sigs.plx
+
+## And provide a searchable index of the superglo corpus
+mkdir -p 02pub/lem
+selemx -p `oraccopt` <01bld/lists/lemindex.lst
+chmod o+r 02pub/lem/*
