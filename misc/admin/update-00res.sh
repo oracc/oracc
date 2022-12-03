268bd291d995da0e16e67043e8d2b3b8fabae5e0
Steve Tinney <stinney@upenn.edu>
2022-12-03 15:01:38 -0500

    add update-00res.sh for Jamie

---
+++ misc/admin/update-00res.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+#
+# Usage:
+#   cd to a project directory
+#   update-00res.sh
+#
+if [ ! -d 00res ]; then
+    echo $0: no 00res directory to update. Stop
+    exit 1
+fi
+p=`oraccopt`
+if [ -d 00res/downloads ]; then
+    echo $0: updating 00res/downloads
+    (cd 00res/downloads ;
+     d=`pwd`
+     cd $ORACC_BUILDS/www/$p/downloads ;
+     rsync -vic $d/* . )
+else
+    echo $0: no 00res/downloads directory, skipping.
+fi
