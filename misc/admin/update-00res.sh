019bdc3354b79675c60b2443c74e18cdf4558cff
Steve Tinney <stinney@upenn.edu>
2022-12-03 15:12:39 -0500

    update-00res.sh use proj-from-conf.sh

--- misc/admin/update-00res.sh
+++ misc/admin/update-00res.sh
@@ -8,7 +8,7 @@ if [ ! -d 00res ]; then
     echo $0: no 00res directory to update. Stop
     exit 1
 fi
-p=`oraccopt`
+p=`proj-from-conf.sh`
 if [ -d 00res/downloads ]; then
     echo $0: updating 00res/downloads
     (cd 00res/downloads ;
