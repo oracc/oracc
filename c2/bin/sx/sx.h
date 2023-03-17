e46b73798a757122e161f620b076c769ddfcd241
stinney <stinney@upenn.edu>
2023-03-17 10:08:15 -0400

    c2 add missing sx.h

---
+++ c2/bin/sx/sx.h
@@ -0,0 +1,8 @@
+#ifndef SX_H_
+#define SX_H_
+
+extern void sx_p_form(Node *np, char *data);
+extern void sx_p_sign(Node *np, char *data);
+extern void sx_p_v(Node *np, char *data);
+
+#endif/*SX_H_*/
