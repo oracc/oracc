0c594d15f6e53b4d67b43dc2fa8172f489800320
Steve Tinney <stinney@upenn.edu>
2023-05-11 23:01:08 +0000

    c2 add surro to gdlstate raw print

--- c2/lib/gdl/gdlstate.c
+++ c2/lib/gdl/gdlstate.c
@@ -8,11 +8,11 @@ gdlstate_t gs_order_f[] = { gs_f_query, gs_f_bang, gs_f_star, gs_f_hash,
 
 gdlstate_t gs_order_o[] = { gs_damaged_o, gs_lost_o, gs_maybe_o, gs_det_o, gs_supplied_o,
 			    gs_excised_o, gs_implied_o, gs_erased_o, gs_cancelled_o,
-			    gs_superposed_o, gs_glolin_o, gs_glodoc_o };
+			    gs_superposed_o, gs_glolin_o, gs_glodoc_o, gs_surro_o };
 
 gdlstate_t gs_order_c[] = { gs_damaged_c, gs_lost_c, gs_maybe_c, gs_det_c, gs_supplied_c,
 			    gs_excised_c, gs_implied_c, gs_erased_c, gs_cancelled_c,
-			    gs_superposed_c, gs_glolin_c, gs_glodoc_c };
+			    gs_superposed_c, gs_glolin_c, gs_glodoc_c, gs_surro_c };
 
 #define NFLAGS (sizeof(gs_order_f)/sizeof(gdlstate_t))
 #define NBRACK (sizeof(gs_order_o)/sizeof(gdlstate_t))
