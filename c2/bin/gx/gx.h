8f8030e5933154916515ddfbbe640b44ba953218
Steve Tinney <stinney@upenn.edu>
2023-06-27 00:19:41 +0000

    c2 rename cx to gx

---
+++ c2/bin/gx/gx.h
@@ -0,0 +1,18 @@
+#ifndef GX_H_
+#define GX_H_ 1
+
+#include <cbd.h>
+
+extern void common_init(void);
+extern void common_term(void);
+extern void untab(unsigned char *s);
+extern unsigned char *tok(unsigned char *s, unsigned char *end);
+extern unsigned char *check_bom(unsigned char *s);
+extern unsigned char **setup_lines(unsigned char *ftext);
+extern void cgp_init(struct cgp *c, unsigned char *cf, unsigned char *gw, unsigned char *pos);
+extern unsigned const char *cgp_entry_str(struct entry *e, int spread);
+extern const unsigned char *cgp_str(struct cgp *cp, int spread);
+extern void cgp_entry(struct cgp *c, struct entry *e);
+extern unsigned char *slurp(const char *caller, const char *fname, ssize_t *fsize);
+
+#endif/*GX_H_*/
