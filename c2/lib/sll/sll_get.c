8e6e238ada4481ff155fa9f0a2e74c87889075ac
Steve Tinney <stinney@upenn.edu>
2023-07-07 18:14:50 -0400

    c2 slx implement key/val pair for sll_get results

--- c2/lib/sll/sll_get.c
+++ c2/lib/sll/sll_get.c
@@ -12,12 +12,34 @@
 #include <gutil.h>
 #include "sll.h"
 
+static char *
+sll_ret_pair(const char *k, const char *oid)
+{
+  char *p = NULL;
+  if (k && oid)
+    {
+      p = malloc(strlen(k) + strlen(oid) + 2);
+      (void)sprintf(p, "%s\t%s", k, oid);
+    }
+  else if (k)
+    {
+      p = malloc(strlen(k) + 3);
+      (void)sprintf(p, "%s\t-", k);
+    }
+  else if (oid)
+    {
+      p = malloc(strlen(oid) + 3);
+      (void)sprintf(p, "-\t%s", oid);
+    }
+  return p;
+}
+
 const char *
 sll_get_name(unsigned char *k)
 {
   const char *oid = (ccp)sll_lookup(k);
   if (oid)
-    return (ccp)sll_lookup(sll_tmp_key((uccp)oid, ""));
+    return sll_ret_pair((ccp)sll_lookup(sll_tmp_key((uccp)oid, "")), oid);
   else
     return NULL;
 }
@@ -25,11 +47,18 @@ sll_get_name(unsigned char *k)
 const char *
 sll_get_oid(unsigned char *k)
 {
-  return (ccp)sll_lookup(k);
+  return (ccp)sll_ret_pair((ccp)k, (ccp)sll_lookup(k));
 }
 
 List *
 sll_get_h(const char *k)
 {
-  return NULL;
+  const char *h = (ccp)sll_lookup(sll_tmp_key((uccp)k, "h"));
+  List *lp = NULL;
+  if (h)
+    {
+      lp = list_create(LIST_SINGLE);
+      list_add(lp, sll_ret_pair(k, h));
+    }
+  return lp;
 }
