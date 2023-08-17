feb83a38a46335611a12a273530ad7c6861cc421
Steve Tinney <stinney@upenn.edu>
2023-08-17 01:40:18 +0000

    c2 sx add missing header

---
+++ c2/bin/sx/pcre2if.h
@@ -0,0 +1,17 @@
+#ifndef PCRE2IF_H_
+#define PCRE2IF_H_
+
+#define PCRE2_CODE_UNIT_WIDTH 8
+#include <pcre2.h>
+
+struct pcre2if_m
+{
+  unsigned char *mstr;
+  size_t off;
+  size_t len;
+};
+
+extern pcre2_code *pcre2if_set_pattern(unsigned const char *pattern);
+extern List *pcre2if_match(pcre2_code *re, const unsigned char *subject, int find_all, Pool *poolp);
+
+#endif/*PCRE2IF_H_*/
