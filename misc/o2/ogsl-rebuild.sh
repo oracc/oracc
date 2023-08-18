46f3bea2b22d9285b24654449e966dab1078509a
Steve Tinney <stinney@upenn.edu>
2023-08-18 14:58:04 +0000

    o2/ogsl-rebuild.sh build suppression

--- misc/o2/ogsl-rebuild.sh
+++ misc/o2/ogsl-rebuild.sh
@@ -2,25 +2,32 @@
 if [ -r .nobuild ]; then
     echo 'You are not allowed to build ogsl here; it can only be built on build-oracc. Stop.'
     exit 1
+else
+    echo '######################################################################'
+    echo '######################################################################'
+    echo '### NOTE: OGSL rebuild is currently suppressed--only the portal    ###'
+    echo '###       is being rebuilt and no oracc serve is being done.       ###'
+    echo '######################################################################'
+    echo '######################################################################'
 fi
-sl-oid.sh
-sl-xml.plx ogsl
-sl-group.plx
-xsltproc ${ORACC}/lib/scripts/sl-letters-file.xsl 02xml/sl-grouped.xml >02pub/letters.tab
-sl-signlist.sh
+#sl-oid.sh
+#sl-xml.plx ogsl
+#sl-group.plx
+#xsltproc ${ORACC}/lib/scripts/sl-letters-file.xsl 02xml/sl-grouped.xml >02pub/letters.tab
+#sl-signlist.sh
 #sl-db.plx ogsl
-sl-db2.plx ogsl
-(cd 02pub/sl ; ln -sf ogsl-db2.tsv sl.tsv)
+#sl-db2.plx ogsl
+#(cd 02pub/sl ; ln -sf ogsl-db2.tsv sl.tsv)
 #echo '#letters' >>02pub/sl/sl.tsv
 #sed 's/	/;let	/' <02pub/letters.tab >>02pub/sl/sl.tsv
 #sl-index -boot
-slix -boot
-xsltproc -stringparam project ogsl ${ORACC}/lib/scripts/sl-HTML.xsl 02xml/ogsl-sl.xml >02www/ogsl.html
+#slix -boot
+#xsltproc -stringparam project ogsl ${ORACC}/lib/scripts/sl-HTML.xsl 02xml/ogsl-sl.xml >02www/ogsl.html
 o2-portal.sh
-cp -p 00lib/alias-*.txt 02pub
-(cd ${ORACC_BUILDS}/xml/ogsl ; rm -f ogsl.xml ; ln -sf ogsl-sl.xml ogsl.xml)
-pubfiles.sh ogsl
-serve-project.sh ogsl
+#cp -p 00lib/alias-*.txt 02pub
+#(cd ${ORACC_BUILDS}/xml/ogsl ; rm -f ogsl.xml ; ln -sf ogsl-sl.xml ogsl.xml)
+#pubfiles.sh ogsl
+#serve-project.sh ogsl
 # host=`hostname`
 # if [[ $host = "build-oracc" ]]; then
 #    echo Rebuilding ogsl on Munich build server ...
