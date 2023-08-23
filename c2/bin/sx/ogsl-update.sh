b44df99e30f0db89fff9c7bc5e3c4c4f4a10f7ef
Steve Tinney <stinney@upenn.edu>
2023-08-23 20:29:38 +0000

    c2 sx fix perms in new ogsl-update.sh

--- c2/bin/sx/ogsl-update.sh
+++ c2/bin/sx/ogsl-update.sh
@@ -4,6 +4,7 @@ sx -c 00lib/ogsl.asl
 if [ $? == 0 ]; then
     sx -b
     slix -b
+    chmod o+r 02pub/sl/*
     echo "ogsl-update.sh: ogsl data updated; web portal unchanged."
 else
     echo "ogsl-update.sh: errors in ogsl processing. Stop."
