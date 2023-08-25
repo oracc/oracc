81be1b3a9f32fd4c2241a01c6b517982561aed57
Steve Tinney <stinney@upenn.edu>
2023-08-25 22:32:33 +0000

    c2 add gvl_bridge.c

---
+++ c2/nss/gdl/gvl_bridge.c
@@ -0,0 +1,57 @@
+#include <hash.h>
+#include <pool.h>
+#include <gdl.h>
+
+static Hash *bridge_h = NULL;
+static Pool *bridge_p = NULL;
+extern int asl_flex_debug , gdl_flex_debug, nwarning;
+
+void
+gvl_bridge_init(void)
+{
+  bridge_h = hash_create(1024);
+  bridge_p = pool_init();
+  (void)gvl_setup("ogsl", "ogsl");
+  gdlparse_init();
+  mesg_init();
+  gdl_flex_debug = 0;
+  gvl_no_mesg_add = 1;
+}
+
+const char *
+gvl_bridge(const char *f,size_t l,const unsigned char *g)
+{
+  Tree *tp = NULL;
+
+  const char *sentinel = "tumₓ(|TUM×GAN₂@t|)";
+  if (!strcmp((ccp)g, sentinel))
+    fprintf(stderr, "gvl_bridge: found %s\n", sentinel);
+  
+  tp = gdlparse_string(mesg_mloc(f,l), (char*)pool_copy(g,bridge_p));
+
+  if (tp && tp->root && tp->root->kids)
+    {
+      gvl_g *gp = tp->root->kids->user;
+      if (gp)
+	{
+	  hash_add(bridge_h, pool_copy(g,bridge_p), gp);
+	  return (ccp)gp->mess;
+	}
+      else if (tp->root->kids->kids)
+	{
+	  /* sexified numbers like '1' have a user == NULL at
+	     root->kids but the sexified version at
+	     root->kids->kids */
+	  if (!strcmp(tp->root->kids->kids->name, "g:n"))
+	    {
+	      gp = tp->root->kids->kids->user;
+	      if (gp)
+		return (ccp)gp->mess;
+	    }
+	}
+    }
+  
+  mesg_verr(mesg_mloc(f,l), "(gvl_bridge) unknown error trying to parse %s", g);
+  ++nwarning;
+  return NULL;
+}
