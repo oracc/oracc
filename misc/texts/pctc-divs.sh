78916d5ed07288754f3222f14393b906b6892b3e
Steve Tinney <stinney@upenn.edu>
2023-09-15 13:08:29 +0000

    c1 less noisy pctc inst processing

--- misc/texts/pctc-divs.sh
+++ misc/texts/pctc-divs.sh
@@ -4,7 +4,7 @@ for a in o* ; do
     (cd $a ;
      for t in *_*.tis ; do
 	 d=`basename $t tis`div
-	 echo processing $t in $a with output to $d
+#	 echo processing $t in $a with output to $d
 	 head -5 <$t | wm -l -p pctc | ce_xtf -3 -p pctc -l | xis-xml-html.sh pctc >$d
      done
     )
--- misc/texts/pctc-signs.sh
+++ misc/texts/pctc-signs.sh
@@ -6,24 +6,28 @@ fi
 rm -fr 01tmp/xis ; mkdir -p 01tmp/xis
 libscripts=$ORACC_BUILDS/lib/scripts
 echo "$0: timing oxx ..."
-time oxx -l01tmp/pctc.log 00atf/*.atf >01tmp/corpus.xml
-echo "$0: timing word-children.xsl ..."
-time xsltproc $libscripts/word-children.xsl 01tmp/corpus.xml | sort | sed 's/^/pctc:/' >01tmp/w-k.tab
+#time
+oxx -l01tmp/pctc.log 00atf/*.atf >01tmp/corpus.xml
+#echo "$0: timing word-children.xsl ..."
+#time
+xsltproc $libscripts/word-children.xsl 01tmp/corpus.xml | sort | sed 's/^/pctc:/' >01tmp/w-k.tab
 cut -f2 01tmp/w-k.tab | gdlx -s -p pctc | cut -f2 >01tmp/oid.col
 cut -f1 01tmp/w-k.tab | paste - 01tmp/oid.col > 01tmp/w-o.tab
 concord -twcl* <01tmp/w-o.tab >01tmp/o-f-w.tab
-echo "$0: pctc-xis.plx ..."
-time pctc-xis.plx <01tmp/o-f-w.tab
-echo "$0: timing pctc-divs.sh ..."
-time pctc-divs.sh
+#echo "$0: pctc-xis.plx ..."
+#time
+pctc-xis.plx <01tmp/o-f-w.tab
+#echo "$0: timing pctc-divs.sh ..."
+#time
+pctc-divs.sh
+#echo "$0: timing pctc-xis-data.sh ..."
+#time 
 (cd 01tmp/xis ;
-echo "$0: timing pctc-xis-data.sh ..."
-time \
-    for a in o* ; do
-    if [ -d $a ]; then
-	(cd $a ;
-	 echo pctc-xis-data.sh $a ...
-	 pctc-xis-data.sh )
-    fi
+ for a in o* ; do
+     if [ -d $a ]; then
+	 (cd $a ;
+	  echo pctc-xis-data.sh $a ...
+	  pctc-xis-data.sh )
+     fi
  done
 )
