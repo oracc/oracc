c673606681fe983626a7e1bc90a9dcbe5f9e1c6d
Steve Tinney <stinney@upenn.edu>
2024-01-07 19:56:16 +0000

    neo-order again

--- misc/neo/neo-order.sh
+++ misc/neo/neo-order.sh
@@ -1,9 +1,11 @@
 #!/bin/dash
 norder=01tmp/neo-order
+norderlem=$norder.lem
 nordertab=$norder.tab
 norderyes=$norder.yes
-rm -f norder
-for a in `cat have-lem` ; do
+rm -f $nordertab $norderyes
+locate lists/have-lem.lst | xargs ls -1 | sed 's#.*/bld/\(.*\)/lists/have-lem.lst#\1#' >$norderlem
+for a in `cat $norderlem` ; do
     printf "%s\t%s\n" $a `oraccopt $a public` >>$nordertab
 done
 grep yes$ $nordertab | cut -f1 >$norderyes
