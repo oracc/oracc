de404352198a5b3d9d65735aec01fc02a35105b8
Steve Tinney <stinney@upenn.edu>
2024-02-04 18:34:09 +0000

    l2p3-install-cbd generate new-style OID table

--- misc/l2phase3/l2p3-install-cbd.sh
+++ misc/l2phase3/l2p3-install-cbd.sh
@@ -34,7 +34,8 @@ fi
 oid=`oraccopt . cbd-oid`
 if [ "$oid" == "yes" ] ; then
     oid-index-cbd.sh `oraccopt`
+    xsltproc ${ORACC_BUILDS}/lib/scripts/cbd-oid-tab.xsl 01bld/*/summaries.xml >02pub/cbd-index.tab
     mkdir -p 02pub/oid
-    dbix -d 02pub/oid -n oid 02pub/oid-index.tab
+    dbix -d 02pub/oid -n oid 02pub/cbd-index.tab
     chmod -R o+r 02pub/oid
 fi
