c226911a39ec03f02a75f82364dfba0540916c74
Steve Tinney <stinney@upenn.edu>
2024-06-02 22:30:41 +0000

    o2 p4 tweaks

--- misc/o2/o2-prepare.sh
+++ misc/o2/o2-prepare.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 o2-cnf.sh
+lx-lists.sh
 o2-lst.sh
+# if [ config-opt=lx-lists ]; (cd 01bld ; mv lists o2lists ; mv lxlists lists)
 o2-cat.sh
 o2-update-lem.sh
 o2-atf.sh
 if [ -r 01bld/destfiles.lst ]; then
-    o2-tr-lst.plx <01bld/destfiles.lst >01bld/lists/have-xtr.tab
+    cut -f1,3 02pub/atf-data.tab | sed 's/^.*://' | sed 's#@[/a-z0-9]\+##'
 fi
+#    o2-tr-lst.plx <01bld/destfiles.lst >01bld/lists/have-xtr.tab
