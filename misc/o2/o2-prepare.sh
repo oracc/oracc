5d536df26d21079dfc4d421f01f32e5f85905df4
Steve Tinney <stinney@upenn.edu>
2024-06-02 23:17:18 +0000

    o2-prepare save have-xtr.tab properly

--- misc/o2/o2-prepare.sh
+++ misc/o2/o2-prepare.sh
@@ -7,6 +7,6 @@ o2-cat.sh
 o2-update-lem.sh
 o2-atf.sh
 if [ -r 01bld/destfiles.lst ]; then
-    cut -f1,3 02pub/atf-data.tab | sed 's/^.*://' | sed 's#@[/a-z0-9]\+##'
+    cut -f1,3 02pub/atf-data.tab | sed 's/^.*://' | sed 's#@[/a-z0-9]\+##' >01bld/lists/have-xtr.tab
 fi
 #    o2-tr-lst.plx <01bld/destfiles.lst >01bld/lists/have-xtr.tab
