a900789646b3cecdce81681772286f88ccecf4cc
Steve Tinney <stinney@upenn.edu>
2024-07-03 11:14:07 +0000

    ox note.c fix static buf error

--- misc/o2/o2-lst.sh
+++ misc/o2/o2-lst.sh
@@ -3,7 +3,4 @@ mkdir -p 01bld/lists
 rm -f 01bld/lists/cat-ids.lst
 xmd-ids.plx 01bld/lists/cat-ids.lst
 o2-lst.plx
-if [ -r 00lib/umbrella.lst ] || [ -r 00lib/search.lst ]; then
-    lx-lists.sh
-fi
 chmod o+r 01bld/lists/*
