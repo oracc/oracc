0967bb6431fa4712804ec177de0ac1da0b9c7b07
Steve Tinney <stinney@upenn.edu>
2024-07-09 19:10:30 +0000

    xmd processing updates

--- misc/o2/o2-cat.sh
+++ misc/o2/o2-cat.sh
@@ -4,7 +4,9 @@ atflinks.plx >01bld/atflinks.tab
 linknorm.plx 01bld/atflinks.tab >01bld/normlinks.tab
 translist.sh >01bld/translist.txt
 xmd-sessions.plx
+echo xmdmanager2.plx
 xmdmanager2.plx
+echo xmd-check.plx
 xmd-check.plx
 xsltproc ${ORACC}/lib/scripts/xmd-summaries.xsl 01bld/cdlicat.xmd >01bld/cat-sum.xml
 xsltproc ${ORACC}/lib/scripts/xmd-HTML-summaries.xsl 01bld/cat-sum.xml >01bld/cat-sum.html
