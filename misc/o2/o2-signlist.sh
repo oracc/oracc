21c04fa191f905a65483f5033d4c76055ddcccf3
Steve Tinney <stinney@upenn.edu>
2024-10-11 11:35:16 +0000

    signlist build improvements

---
+++ misc/o2/o2-signlist.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+echo $0 calling odo-signlist.sh
+odo-signlist.sh o2
+#project=`oraccopt`
+#o2-portal.sh
+#cp -p 00lib/alias-*.txt 02pub
+#if [ -d 01bld/snippets ]; then
+#    cp -a 01bld/snippets 02www
+#fi
+#pubfiles.sh $project
